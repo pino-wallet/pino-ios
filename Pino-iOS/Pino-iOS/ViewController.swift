dc70ab3996a60a5ab35ad169ca5a28d8615df975
Sobhan Eskandari <eskandari.sobhan@gmail.com>
2022-11-05 10:57:44 +0330

    feat: init new pino project

---
+++ Pino-iOS/Pino-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pino-iOS
+//
+//  Created by Sobhan Eskandari on 11/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
