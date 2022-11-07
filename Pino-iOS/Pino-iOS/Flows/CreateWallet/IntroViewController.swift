35f91ee93d185425934160f165a0bf94e8ecccba
Sobhan Eskandari <eskandari.sobhan@gmail.com>
2022-11-07 19:38:36 +0330

    feat: remove storyboard and load init view controller from introview temporarily

---
+++ Pino-iOS/Pino-iOS/Flows/CreateWallet/IntroViewController.swift
@@ -0,0 +1,28 @@
+//
+//  IntroViewController.swift
+//  Pino-iOS
+//
+//  Created by Sobhan Eskandari on 11/7/22.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
