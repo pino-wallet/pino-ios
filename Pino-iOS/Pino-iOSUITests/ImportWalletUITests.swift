54f4da00f27dda2591313f8159936196320a5c8c
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2022-11-29 14:03:04 +0330

    test: add test class for import wallet

--- Pino-iOS/Pino-iOSUITests/ImportWalletUITests.swift
+++ Pino-iOS/Pino-iOSUITests/ImportWalletUITests.swift
@@ -23,7 +23,7 @@ final class ImportWalletUITests: XCTestCase {
 		// Put teardown code here. This method is called after the invocation of each test method in the class.
 	}
 
-	func testTypeSecretPhrase() throws {
+	func testImportSecretPhrase() throws {
 		// UI tests must launch the application that they test.
 		let app = XCUIApplication()
 		app.launch()
