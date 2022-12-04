a7ee92a926dd904a58a0a3c55e24009d20e23bad
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2022-12-04 15:12:02 +0330

    refactor: apply changes in import wallet UI tests

--- Pino-iOS/Pino-iOSUITests/CreatePasscodeUITests.swift
+++ Pino-iOS/Pino-iOSUITests/CreatePasscodeUITests.swift
@@ -9,11 +9,56 @@ import XCTest
 
 final class CreatePasscodeUITests: XCTestCase {
 	let app = XCUIApplication()
+	let errorLabel = XCUIApplication().staticTexts["Incorrect, try again!"]
+	let testPassCode = ["1", "2", "3", "4", "5", "6"]
+	let invalidPasscode = ["1", "2", "3", "4", "5", "0"]
+
 	override func setUpWithError() throws {
 		continueAfterFailure = false
 	}
 
 	override func tearDownWithError() throws {}
 
-	func testCreatePassword() {}
+	func testPasscode() throws {
+		let importWalletUiTest = ImportWalletUITests()
+		importWalletUiTest.openImportWalletPage()
+		importWalletUiTest.openKeyboard()
+		app.typeText(importWalletUiTest.testSecretPhrase.joined(separator: " "))
+		app.tap()
+		importWalletUiTest.importButton.tap()
+		createPasscode()
+		verifyInvalidPasscode()
+		verifyValidPasscode()
+	}
+
+	func createPasscode() {
+		sleep(1)
+		for number in testPassCode {
+			app.keys[number].tap()
+		}
+	}
+
+	func verifyInvalidPasscode() {
+		sleep(1)
+		for number in invalidPasscode {
+			app.keys[number].tap()
+		}
+		testErrorDisplay(true)
+	}
+
+	func verifyValidPasscode() {
+		sleep(1)
+		for number in testPassCode {
+			app.keys[number].tap()
+			testErrorDisplay(false)
+		}
+	}
+
+	func testErrorDisplay(_ isDisplay: Bool) {
+		if isDisplay {
+			XCTAssertTrue(errorLabel.exists)
+		} else {
+			XCTAssertFalse(errorLabel.exists)
+		}
+	}
 }
--- Pino-iOS/Pino-iOSUITests/ImportWalletUITests.swift
+++ Pino-iOS/Pino-iOSUITests/ImportWalletUITests.swift
@@ -10,6 +10,9 @@ import XCTest
 final class ImportWalletUITests: XCTestCase {
 	let app = XCUIApplication()
 	var importButton: XCUIElement!
+	let testSecretPhrase = Array(MockSeedPhrase.wordList.shuffled().prefix(12))
+	let secretPhraseThirteenthWord = "thirteenth"
+
 	override func setUpWithError() throws {
 		continueAfterFailure = false
 	}
@@ -20,115 +23,66 @@ final class ImportWalletUITests: XCTestCase {
 
 	func testTypeSecretPhrase() throws {
 		openImportWalletPage()
-		// Test import button activation and deactivation
-		let seedPhraseTextField = app.textViews.element(boundBy: 0)
-		seedPhraseTextField.tap()
-		app.typeText("a ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("b ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("c ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("d ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("e ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("f ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("g ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("h ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("i ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("j ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("k ")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("l")
-		XCTAssertTrue(importButton.isEnabled)
-		app.typeText(" ")
-		XCTAssertTrue(importButton.isEnabled)
-		app.typeText("m")
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText(XCUIKeyboardKey.delete.rawValue)
-		app.typeText(XCUIKeyboardKey.return.rawValue)
-		importButton.tap()
+		openKeyboard()
+		for (index, word) in testSecretPhrase.enumerated() {
+			// Type words of the secret phrase
+			app.typeText("\(word) ")
+			if index == 11 {
+				// Words count is 12
+				testImportButtonActivation(true)
+				// Type 13th word
+				app.typeText(secretPhraseThirteenthWord)
+				testImportButtonActivation(false)
+				// delete 13th word
+				for _ in secretPhraseThirteenthWord {
+					testImportButtonActivation(false)
+					app.typeText(XCUIKeyboardKey.delete.rawValue)
+				}
+				// Words count is 12 again
+				testImportButtonActivation(true)
+			} else {
+				// Words count is less than 12
+				testImportButtonActivation(false)
+			}
+		}
+
+		openCreatePasscode()
 	}
 
 	func testSelectSuggestedWord() throws {
 		openImportWalletPage()
-		let seedPhraseTextField = app.textViews.element(boundBy: 0)
-		seedPhraseTextField.tap()
+		openKeyboard()
 
-		app.typeText("a")
-		let firstSuggestedWord = app.collectionViews.element(boundBy: 0).cells.element(boundBy: 0)
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("b")
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("c")
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("d")
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("e")
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("f")
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("g")
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("h")
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("i")
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("j")
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("k")
-		firstSuggestedWord.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		app.typeText("l")
-		firstSuggestedWord.tap()
-		XCTAssertTrue(importButton.isEnabled)
-		app.tap()
-		importButton.tap()
-		createPasscode()
-		//        app.typeText("m")
-//		firstSuggestedWord.tap()
-//		XCTAssertFalse(importButton.isEnabled)
-	}
+		let testWordsFirstLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"]
 
-	func createPasscode() {
-		app.keys["1"].tap()
-		app.keys["2"].tap()
-		app.keys["3"].tap()
-		app.keys["4"].tap()
-		app.keys["5"].tap()
-		app.keys["6"].tap()
-		sleep(1)
+		for (index, firstLetter) in testWordsFirstLetter.enumerated() {
+			// Type first letter of word
+			app.typeText(firstLetter)
+			// Select first suggested word
+			let firstSuggestedWord = app.collectionViews.element(boundBy: 0).cells.element(boundBy: 0)
+			firstSuggestedWord.tap()
+			if index == 11 {
+				// Words count is 12
+				testImportButtonActivation(true)
+				// Type 13th word
+				for char in secretPhraseThirteenthWord {
+					app.typeText(String(char))
+				}
+				testImportButtonActivation(false)
+				// delete 13th word
+				for _ in secretPhraseThirteenthWord {
+					testImportButtonActivation(false)
+					app.typeText(XCUIKeyboardKey.delete.rawValue)
+				}
+				// Words count is 12 again
+				testImportButtonActivation(true)
+			} else {
+				// Words count is less than 12
+				testImportButtonActivation(false)
+			}
+		}
 
-		app.keys["1"].tap()
-		app.keys["2"].tap()
-		app.keys["3"].tap()
-		app.keys["4"].tap()
-		app.keys["5"].tap()
-		app.keys["0"].tap()
-		sleep(1)
-
-		app.keys["1"].tap()
-		app.keys["2"].tap()
-		app.keys["3"].tap()
-		app.keys["4"].tap()
-		app.keys["5"].tap()
-		app.keys["6"].tap()
+		openCreatePasscode()
 	}
 
 	func testPasteSecretPhrase() throws {
@@ -136,17 +90,19 @@ final class ImportWalletUITests: XCTestCase {
 		let pasteButton = app.buttons["Paste"]
 
 		// Paste 11 words
-		UIPasteboard.general.string = "a b c d e f g h i j k "
+		UIPasteboard.general.string = Array(testSecretPhrase.prefix(11)).joined(separator: " ")
 		pasteButton.tap()
-		XCTAssertFalse(importButton.isEnabled)
-		// Paste 12 words
-		UIPasteboard.general.string = "a b c d e f g h i j k l"
-		pasteButton.tap()
-		XCTAssertTrue(importButton.isEnabled)
+		testImportButtonActivation(false)
 		// Paste 13 words
-		UIPasteboard.general.string = "a b c d e f g h i j k l m"
+		UIPasteboard.general.string = testSecretPhrase.joined(separator: " ") + " thirteenth"
 		pasteButton.tap()
-		XCTAssertFalse(importButton.isEnabled)
+		testImportButtonActivation(false)
+		// Paste 12 words
+		UIPasteboard.general.string = testSecretPhrase.joined(separator: " ")
+		pasteButton.tap()
+		testImportButtonActivation(true)
+
+		openCreatePasscode()
 	}
 
 	func openImportWalletPage() {
@@ -156,6 +112,27 @@ final class ImportWalletUITests: XCTestCase {
 		let importWalletButton = app.buttons.element(boundBy: 1)
 		importWalletButton.tap()
 		importButton = app.buttons["Import"]
-		XCTAssertFalse(importButton.isEnabled)
+		testImportButtonActivation(false)
+	}
+
+	func openKeyboard() {
+		let seedPhraseTextField = app.textViews.element(boundBy: 0)
+		seedPhraseTextField.tap()
+	}
+
+	func testImportButtonActivation(_ isActive: Bool) {
+		if isActive {
+			XCTAssertTrue(importButton.isEnabled)
+		} else {
+			XCTAssertFalse(importButton.isEnabled)
+		}
+	}
+
+	func openCreatePasscode() {
+		app.tap()
+		importButton.tap()
+		let createPasscodeUITest = CreatePasscodeUITests()
+		createPasscodeUITest.createPasscode()
+		createPasscodeUITest.verifyValidPasscode()
 	}
 }
