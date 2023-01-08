8e34c027ecdaa45cff796499b2b01013f8edfa8d
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2023-01-08 15:53:27 +0330

    test: add security mode ui test

--- Pino-iOS/Pino-iOSUITests/Flows/HomepageUITests.swift
+++ Pino-iOS/Pino-iOSUITests/Flows/HomepageUITests.swift
@@ -36,4 +36,35 @@ final class HomepageUITests: XCTestCase {
 		app.navigationBars.element(boundBy: 0).buttons.element(boundBy: 1).tap()
 		XCTAssertNotNil(UIPasteboard.general.string)
 	}
+
+	internal func testSecurityMode() {
+		let balanceLabel = app.staticTexts.element(boundBy: 2)
+		let showBalanceButton = app.staticTexts["Show balance"]
+		app.launch()
+		balanceLabel.tap()
+		XCTAssertEqual(balanceLabel.label, securityText)
+		XCTAssert(showBalanceButton.exists)
+		checkAssetsSecurityMode(isEnabled: true)
+		balanceLabel.tap()
+		XCTAssertNotEqual(balanceLabel.label, securityText)
+		checkAssetsSecurityMode(isEnabled: false)
+	}
+
+	// MARK: Private Methods
+
+	private func checkAssetsSecurityMode(isEnabled: Bool) {
+		for index in 0 ..< assets.count {
+			let asset = assets.element(boundBy: index)
+			if isEnabled {
+				XCTAssertEqual(asset.staticTexts.element(boundBy: 0).label, securityText)
+				XCTAssertEqual(asset.staticTexts.element(boundBy: 1).label, securityText)
+				XCTAssertEqual(asset.staticTexts.element(boundBy: 3).label, securityText)
+			} else {
+				XCTAssertNotEqual(asset.staticTexts.element(boundBy: 0).label, securityText)
+				XCTAssertNotEqual(asset.staticTexts.element(boundBy: 1).label, securityText)
+				XCTAssertNotEqual(asset.staticTexts.element(boundBy: 3).label, securityText)
+			}
+			XCTAssertNotEqual(asset.staticTexts.element(boundBy: 2).label, securityText)
+		}
+	}
 }
