fb76c6e9e97760a99e5a4e52c21bcb313cf51ec9
Sobhan Eskandari <eskandari.sobhan@gmail.com>
2023-01-19 15:34:14 +0330

    style: run swift formatter

--- Pino-iOS/Pino-iOSTests/Networking/APIClientTest.swift
+++ Pino-iOS/Pino-iOSTests/Networking/APIClientTest.swift
@@ -6,138 +6,136 @@
 //
 
 import Combine
-import XCTest
 import OHHTTPStubs
 import OHHTTPStubsSwift
-
 @testable import Pino_iOS
+import XCTest
+
 final class APIClientTest: XCTestCase {
 	// MARK: - Properties
 
-    private var apiClient: APIClient!
+	private var apiClient: APIClient!
 	private var subscriptions: Set<AnyCancellable> = []
-    private var stubsDescriptors: [HTTPStubsDescriptor] = []
+	private var stubsDescriptors: [HTTPStubsDescriptor] = []
 
 	override func setUpWithError() throws {
 		// Put setup code here. This method is called before the invocation of each test method in the class.
-        apiClient = APIClient(keychainService: MockKeychainService())
+		apiClient = APIClient(keychainService: MockKeychainService())
 	}
 
 	override func tearDownWithError() throws {
 		// Put teardown code here. This method is called after the invocation of each test method in the class.
-        stubsDescriptors.forEach { stub in
-            HTTPStubs.removeStub(stub)
-        }
+		stubsDescriptors.forEach { stub in
+			HTTPStubs.removeStub(stub)
+		}
 	}
 
 	func testTransactions_Success() {
-        runTransactionsTests(statusCode: 201)
+		runTransactionsTests(statusCode: 201)
 	}
 
-    func testTransactions_Failure() {
-        runTransactionsTests(statusCode: 401)
-    }
+	func testTransactions_Failure() {
+		runTransactionsTests(statusCode: 401)
+	}
 
-    func testTransactions_NotFound() {
-        runTransactionsTests(statusCode: 404)
-    }
+	func testTransactions_NotFound() {
+		runTransactionsTests(statusCode: 404)
+	}
 
-    
-    func runTransactionsTests(statusCode: StatusCode) {
-        let endPoint = Endpoint.transactions
-        stubAPI(endPoint: endPoint,
-                statusCode: statusCode,
-                response: .file(name: endPoint.stubPath))
-        .store(in: &stubsDescriptors)
+	func runTransactionsTests(statusCode: StatusCode) {
+		let endPoint = Endpoint.transactions
+		stubAPI(
+			endPoint: endPoint,
+			statusCode: statusCode,
+			response: .file(name: endPoint.stubPath)
+		)
+		.store(in: &stubsDescriptors)
 
-        let expectation = XCTestExpectation(description: "Fetch transactions")
+		let expectation = XCTestExpectation(description: "Fetch transactions")
 
-        apiClient.transactions().sink(receiveCompletion: { completion in
-            switch completion {
-            case .finished:
-                if !statusCode.isSuccess {
-                    XCTFail("Req failed")
-                }
-            case .failure(let error):
-                if !statusCode.isSuccess {
-                    XCTFail("Req failed")
-                }
-                switch statusCode {
-                case 401:
-                    XCTAssertEqual(error, APIError.unauthorized)
-                default:
-                    XCTAssertEqual(error, APIError.failedRequest)
-                }
-            }
-            expectation.fulfill()
-        }) { transactions in
-            XCTAssertNotNil(transactions)
-        }.store(in: &subscriptions)
+		apiClient.transactions().sink(receiveCompletion: { completion in
+			switch completion {
+			case .finished:
+				if !statusCode.isSuccess {
+					XCTFail("Req failed")
+				}
+			case let .failure(error):
+				if !statusCode.isSuccess {
+					XCTFail("Req failed")
+				}
+				switch statusCode {
+				case 401:
+					XCTAssertEqual(error, APIError.unauthorized)
+				default:
+					XCTAssertEqual(error, APIError.failedRequest)
+				}
+			}
+			expectation.fulfill()
+		}) { transactions in
+			XCTAssertNotNil(transactions)
+		}.store(in: &subscriptions)
 
-        waitForExpectations(timeout: 1.5)
-    }
+		waitForExpectations(timeout: 1.5)
+	}
 }
 
 fileprivate enum Endpoint {
-    
-    // MARK: - Cases
-    case transactions
-    
-    var path: String {
-        switch self {
-        case .transactions:
-            return "transactions"
-        }
-    }
-    
-    var stubPath: String {
-        switch self {
-        case .transactions:
-            return "transactions-mock"
-        }
-    }
-    
+	// MARK: - Cases
+
+	case transactions
+
+	var path: String {
+		switch self {
+		case .transactions:
+			return "transactions"
+		}
+	}
+
+	var stubPath: String {
+		switch self {
+		case .transactions:
+			return "transactions-mock"
+		}
+	}
 }
 
-fileprivate extension APIClientTest {
-    
-    // MARK: - Types
-    
-    enum Response {
-        case data(data: Data)
-        case file(name: String)
-    }
-    
-    //MARK: - Stub API
-    
-    func simulateFailure(endPoint: Endpoint, error: Error) -> HTTPStubsDescriptor {
-        stub { request in
-            request.url?.path == endPoint.path
-        } response: { _ in
-            HTTPStubsResponse(error: error)
-        }
-    }
-    
-    func stubAPI(endPoint: Endpoint, statusCode: StatusCode, response: Response) -> HTTPStubsDescriptor {
-        stub { request in
-            request.url?.path == endPoint.path
-        } response: { request in
-            if statusCode.isSuccess {
-                switch response {
-                case .data(let data):
-                    return HTTPStubsResponse(data: data, statusCode: statusCode, headers: nil)
-                case .file(let name):
-                    if let stubPath = OHPathForFile(name, type(of: self)) {
-                        return fixture(filePath: stubPath, status: 200, headers: [:])
-                    } else {
-                        fatalError("Stub file not found")
-                    }
-                }
-                
-            } else {
-                return HTTPStubsResponse(data: Data(), statusCode: Int32(statusCode), headers: [:])
-            }
-        }
-    }
-    
+extension APIClientTest {
+	// MARK: - Types
+
+	fileprivate enum Response {
+		case data(data: Data)
+		case file(name: String)
+	}
+
+	// MARK: - Stub API
+
+	fileprivate func simulateFailure(endPoint: Endpoint, error: Error) -> HTTPStubsDescriptor {
+		stub { request in
+			request.url?.path == endPoint.path
+		} response: { _ in
+			HTTPStubsResponse(error: error)
+		}
+	}
+
+	fileprivate func stubAPI(endPoint: Endpoint, statusCode: StatusCode, response: Response) -> HTTPStubsDescriptor {
+		stub { request in
+			request.url?.path == endPoint.path
+		} response: { request in
+			if statusCode.isSuccess {
+				switch response {
+				case let .data(data):
+					return HTTPStubsResponse(data: data, statusCode: statusCode, headers: nil)
+				case let .file(name):
+					if let stubPath = OHPathForFile(name, type(of: self)) {
+						return fixture(filePath: stubPath, status: 200, headers: [:])
+					} else {
+						fatalError("Stub file not found")
+					}
+				}
+
+			} else {
+				return HTTPStubsResponse(data: Data(), statusCode: Int32(statusCode), headers: [:])
+			}
+		}
+	}
 }
