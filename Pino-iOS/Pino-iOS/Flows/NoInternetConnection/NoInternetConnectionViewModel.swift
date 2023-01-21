f1c9dda84c8528bf0221dd18fe16a44743a69027
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2023-01-21 12:13:38 +0330

    feat: add no internet connection view model

--- Pino-iOS/Pino-iOS/Flows/NoInternetConnection/NoInternetConnectionViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/NoInternetConnection/NoInternetConnectionViewModel.swift
@@ -4,3 +4,9 @@
 //
 //  Created by Mohi Raoufi on 1/21/23.
 //
+struct NoInternetConnectionViewModel {
+	public let title = "No internet connection"
+	public let description = "Something went wrong! Unable to fetch coin info. Please try again later"
+	public let image = "info.circle"
+	public let RetryButtonTitle = "Retry"
+}
