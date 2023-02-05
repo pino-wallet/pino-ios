072b2c12bfec2b0bcd927b294c3114129f9a4901
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2023-02-05 10:38:55 +0330

    fix: foldering problem

---
+++ Pino-iOS/Pino-iOS/Flows/Wallet/ImportWallet/ViewController/ImportSecretPhraseViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ImportSecretPhraseViewController.swift
+//  Pino-iOS
+//
+//  Created by Mohi Raoufi on 2/5/23.
+//
+
+import UIKit
+
+class ImportSecretPhraseViewController: UIViewController {
+    // MARK: PublicProperties
+    
+    public var importsecretPhraseView: ImportSecretPhraseView!
+    public var validationSecretPhraseVM: ValidateSecretPhraseViewModel!
+    
+    // MARK: View Overrides
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func loadView() {
+        setupView()
+        setSteperView(stepsCount: 2, curreuntStep: 1)
+    }
+    
+    // MARK: Private Methods
+    
+    private func setupView() {
+        validationSecretPhraseVM = ValidateSecretPhraseViewModel()
+        importsecretPhraseView = ImportSecretPhraseView(validationPharaseVM: validationSecretPhraseVM)
+        addButtonsAction()
+        view = importsecretPhraseView
+    }
+    
+    private func addButtonsAction() {
+        importsecretPhraseView.importButton.addAction(UIAction(handler: { _ in
+            self.validationSecretPhraseVM.validate(
+                secretPhrase: self.importsecretPhraseView.seedPhrasetextView.seedPhraseArray,
+                onSuccess: {
+                    self.importWallet()
+                },
+                onFailure: { validationError in
+                    switch validationError {
+                    case .invalidSecretPhrase:
+                        self.importsecretPhraseView?.showError()
+                    }
+                }
+            )
+        }), for: .touchUpInside)
+    }
+    
+    private func importWallet() {
+        // Go to create passcode page
+        let createPasscodeViewController = CreatePasscodeViewController()
+        createPasscodeViewController.pageSteps = 2
+        navigationController?.pushViewController(createPasscodeViewController, animated: true)
+    }
+}
