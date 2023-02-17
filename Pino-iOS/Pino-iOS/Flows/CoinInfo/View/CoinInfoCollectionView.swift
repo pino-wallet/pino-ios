72ee6d306a1f80cd6ccc9c65b40355f5ca5e1f86
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2023-02-17 14:11:24 +0330

    feat: add coin info collection view

---
+++ Pino-iOS/Pino-iOS/Flows/CoinInfo/View/CoinInfoCollectionView.swift
@@ -0,0 +1,157 @@
+//
+//  CoinInfoCollectionView.swift
+//  Pino-iOS
+//
+//  Created by Mohi Raoufi on 2/17/23.
+//
+
+import Combine
+import UIKit
+
+class CoinInfoCollectionView: UICollectionView {
+	// MARK: - Private Properties
+
+	private var cacncellabel = Set<AnyCancellable>()
+	private let historyRefreshContorl = UIRefreshControl()
+	private let refreshErrorTostView = PinoToastView(message: nil, style: .secondary)
+
+	// MARK: - Internal Properties
+
+	internal var coinInfoVM: CoinInfoPageViewModel!
+
+	// MARK: - Initializers
+
+	#warning("need to refactor")
+	init(coinInfoVM: CoinInfoViewModel) {
+		self.coinInfoVM = coinInfoVM
+		let flowLayout = UICollectionViewFlowLayout(scrollDirection: .vertical)
+		super.init(frame: .zero, collectionViewLayout: flowLayout)
+		configCollectionView()
+		setupView()
+		setUpStyle()
+		setupBinding()
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError()
+	}
+
+	// MARK: - Private Methods
+
+	private func configCollectionView() {
+		register(
+			CoinInfoHeaderView.self,
+			forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+			withReuseIdentifier: CoinInfoHeaderView.headerReuseID
+		)
+		register(
+			CoinHistoryCell.self,
+			forCellWithReuseIdentifier: CoinHistoryCell.cellID
+		)
+		dataSource = self
+		delegate = self
+	}
+
+	private func setupView() {
+		setupRefreshControl()
+	}
+
+	private func setUpStyle() {
+		backgroundColor = .Pino.background
+		showsVerticalScrollIndicator = false
+	}
+
+	private func setupBinding() {
+		coinInfoVM.$historyList.sink { [weak self] _ in
+			self?.reloadData()
+		}.store(in: &cacncellabel)
+	}
+
+	private func setupRefreshControl() {
+		indicatorStyle = .white
+		historyRefreshContorl.tintColor = .Pino.green2
+		historyRefreshContorl.addAction(UIAction(handler: { _ in
+			self.refreshData()
+		}), for: .valueChanged)
+		refreshControl = historyRefreshContorl
+	}
+
+	private func refreshData() {
+		coinInfoVM.refreshCoinInfoData { error in
+			self.refreshControl?.endRefreshing()
+			if let error {
+				switch error {
+				case .requestFaild:
+					self.refreshErrorTostView.message = self.coinInfoVM.requestFailedErrorToastMessage
+				case .networkingConnection:
+					self.refreshErrorTostView.message = self.coinInfoVM.connectionErrorToastMessage
+				}
+				self.refreshErrorTostView.showToast()
+			}
+		}
+	}
+}
+
+// MARK: - CollectionView Flow Layout
+
+extension CoinInfoCollectionView: UICollectionViewDelegateFlowLayout {
+	func collectionView(
+		_ collectionView: UICollectionView,
+		layout collectionViewLayout: UICollectionViewLayout,
+		sizeForItemAt indexPath: IndexPath
+	) -> CGSize {
+		CGSize(width: collectionView.frame.width, height: 72)
+	}
+}
+
+// MARK: - CollectionView DataSource
+
+extension CoinInfoCollectionView: UICollectionViewDataSource {
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		coinInfoVM.historyList.count
+	}
+
+	func collectionView(
+		_ collectionView: UICollectionView,
+		viewForSupplementaryElementOfKind kind: String,
+		at indexPath: IndexPath
+	) -> UICollectionReusableView {
+		let coinInfoHeaderView = dequeueReusableSupplementaryView(
+			ofKind: kind,
+			withReuseIdentifier: CoinInfoHeaderView.headerReuseID,
+			for: indexPath
+		) as! CoinInfoHeaderView
+		coinInfoHeaderView.coinInfoVM = coinInfoVM
+		return coinInfoHeaderView
+	}
+
+	func collectionView(
+		_ collectionView: UICollectionView,
+		layout collectionViewLayout: UICollectionViewLayout,
+		referenceSizeForHeaderInSection section: Int
+	) -> CGSize {
+		let indexPath = IndexPath(row: 0, section: section)
+		let headerView = self.collectionView(
+			collectionView,
+			viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionHeader,
+			at: indexPath
+		)
+		return headerView.systemLayoutSizeFitting(
+			CGSize(width: collectionView.frame.width, height: UIView.layoutFittingExpandedSize.height),
+			withHorizontalFittingPriority: .required,
+			verticalFittingPriority: .fittingSizeLevel
+		)
+	}
+
+	func collectionView(
+		_ collectionView: UICollectionView,
+		cellForItemAt indexPath: IndexPath
+	) -> UICollectionViewCell {
+		let coinHistoryCell = dequeueReusableCell(
+			withReuseIdentifier: CoinHistoryCell.cellID,
+			for: indexPath
+		) as! HistoryCollectionViewCell
+		coinHistoryCell.historyCoinInfoVM = coinInfoVM.historyList[indexPath.row]
+		return coinHistoryCell
+	}
+}
