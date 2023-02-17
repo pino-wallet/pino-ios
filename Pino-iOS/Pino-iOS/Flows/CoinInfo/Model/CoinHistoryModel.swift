b9e3c0cb81034ed08a961203f5af840065bb0d1f
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2023-02-17 14:34:03 +0330

    feat: add models to coin info page

---
+++ Pino-iOS/Pino-iOS/Flows/CoinInfo/Model/CoinHistoryModel.swift
@@ -0,0 +1,15 @@
+//
+//  CoinHistoryModel.swift
+//  Pino-iOS
+//
+//  Created by Mohi Raoufi on 2/17/23.
+//
+
+public struct CoinHistoryModel {
+	// MARK: - Public Properties
+
+	public var icon: String
+	public var title: String
+	public var time: String
+	public var status: String
+}
---
+++ Pino-iOS/Pino-iOS/Flows/CoinInfo/Model/CoinPortfolioModel.swift
@@ -0,0 +1,20 @@
+//
+//  CoinPortfolioModel.swift
+//  Pino-iOS
+//
+//  Created by Mohi Raoufi on 2/17/23.
+//
+
+struct CoinPortfolioModel {
+	// MARK: - public properties
+
+	public var assetImage: String
+	public var userAmount: String
+	public var coinAmount: String
+	public var name: String
+	public var volatilityRate: String
+	public var volatilityType: String
+	public var investAmount: String
+	public var collateralAmount: String
+	public var barrowAmount: String
+}
