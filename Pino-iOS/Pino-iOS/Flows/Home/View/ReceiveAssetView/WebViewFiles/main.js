c973154450b0c09badcbf486faa591c2e9252c64
Amirrk79 <amirmntmntmntmnt@gmail.com>
2023-03-27 13:41:37 +0330

    fix: update webview files

--- Pino-iOS/Pino-iOS/Flows/Home/View/ReceiveAssetView/WebViewFiles/main.js
+++ Pino-iOS/Pino-iOS/Flows/Home/View/ReceiveAssetView/WebViewFiles/main.js
@@ -1,23 +1,26 @@
 
+let timeout;
 
-function generateAndShowQRCode(qrCode) {
-               let qrCodeComponent = document.getElementById("qr1")
+ function generateAndShowQRCode(qrCode) {
+     clearTimeout(timeout)
+         let qrCodeComponent = document.getElementById("qr1")
                qrCodeComponent.contents = qrCode
-    setTimeout(() => {
-            animateQRCode(qrCodeComponent)
-    } , 20)
+     
+    timeout = setTimeout(() => {
+     animateQRCode(qrCodeComponent)
+     }, 0)
+
            }
+
 function animateQRCode(qrCodeComponent) {
     qrCodeComponent.style = `${qrCodeComponent.style} display: block;`
     qrCodeComponent
     .animateQRCode((targets, _x, _y, _count, entity) => ({
       targets,
       from: entity === "module" ? Math.random() * 1000 : 500,
-      duration: 1000,
+      duration: 400,
       web: {
         opacity: entity === "module" ? [0, 0.5, 0, 1] : [0, 1]
       }
     }));
 }
-
-
