590f22e24529a814ad29e64fbb0b96a150e90b84
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2023-04-09 13:15:31 +0330

    feat: add fallback to face id authentication

--- Pino-iOS/Pino-iOS/Utilities/BiometricAuthentication.swift
+++ Pino-iOS/Pino-iOS/Utilities/BiometricAuthentication.swift
@@ -9,7 +9,7 @@ import LocalAuthentication
 import UIKit
 
 class AuthenticationLockViewController: UIViewController {
-	private var lockMethod = LockMethodType.passcode
+	private var lockMethod = LockMethodType.face_id
 
 	public func unlockApp(onSuccess: @escaping () -> Void) {
 		switch lockMethod {
@@ -26,35 +26,60 @@ class AuthenticationLockViewController: UIViewController {
 
 	private func unlockWithBiometric(onSuccess: @escaping () -> Void) {
 		var biometricAuthentication = BiometricAuthentication()
-		biometricAuthentication.evaluate {
-			onSuccess()
-		}
+		biometricAuthentication.evaluate(
+			onSuccess: {
+				onSuccess()
+			},
+			onFailure: { errorCode in
+				switch errorCode {
+				case LAError.userFallback.rawValue:
+					self.unlockWithPasscode {
+						onSuccess()
+					}
+				case LAError.userCancel.rawValue:
+					print("User cancel authentication")
+				default:
+					self.showFailureAlert()
+				}
+			}
+		)
 	}
 
 	private func unlockWithPasscode(onSuccess: @escaping () -> Void) {
 		let unlockAppVC = UnlockAppViewController(
 			onSuccessUnlock: {
 				onSuccess()
+				self.dismiss(animated: true)
 			},
 			onFaceIDFallback: {
 				self.unlockWithBiometric {
 					onSuccess()
+					self.dismiss(animated: true)
 				}
 			}
 		)
 		unlockAppVC.modalPresentationStyle = .fullScreen
 		present(unlockAppVC, animated: true)
 	}
+
+	private func showFailureAlert() {
+		let alertVC = AlertHelper.alertController(
+			title: "Warning!",
+			message: "There was a problem. Please try again",
+			actions: [.gotIt()]
+		)
+		present(alertVC, animated: true)
+	}
 }
 
 extension AuthenticationLockViewController {
 	struct BiometricAuthentication {
 		private let laContext = LAContext()
-		private let biometricsPolicy = LAPolicy.deviceOwnerAuthentication
+		private let biometricsPolicy = LAPolicy.deviceOwnerAuthenticationWithBiometrics
 		private var localizedReason = "Unlock device"
 		private var error: NSError?
 
-		public mutating func evaluate(onSuccess: @escaping () -> Void) {
+		public mutating func evaluate(onSuccess: @escaping () -> Void, onFailure: @escaping (Int) -> Void) {
 			if canEvaluate() {
 				laContext.evaluatePolicy(
 					biometricsPolicy,
@@ -64,14 +89,14 @@ extension AuthenticationLockViewController {
 							if isSuccess {
 								onSuccess()
 							} else {
-								#warning("Talk to designer about handling failed auth")
-								print(error?.localizedDescription ?? "Authentication failed")
+								guard let error else { return }
+								onFailure(error._code)
 							}
 						}
 					}
 				)
 			} else {
-				fatalError("Auth evaluation failed")
+				onFailure(LAError.authenticationFailed.rawValue)
 			}
 		}
 
