b4df62d06efa29e48111b7dea5545cccf524d4c8
sobhaneskandari <eskandari.sobhan@gmail.com>
2023-04-15 19:25:45 +0330

    feat: add secure enclave to secure data

--- Pino-iOS/Pino-iOS/Foundation/SecureEnclave/KeychainManager.swift
+++ Pino-iOS/Pino-iOS/Foundation/SecureEnclave/KeychainManager.swift
@@ -5,101 +5,117 @@
 //  Created by Sobhan Eskandari on 4/13/23.
 //  Based on https://github.com/algrid/keychain-sample
 
+import Combine
 import Foundation
 import LocalAuthentication
-import Combine
 
 class KeychainManager {
-    
-    // MARK: - Private Initializer
+	// MARK: - Private Initializer
 
-    private init() {}
-    
-    // MARK: - Private Properties
+	private init() {}
 
-    private static let bitSize:Int = 256
-    
-    // MARK: - Create and store keys in Keychain
-    
-    static func createPrivateKey(name: String) throws -> SecKey {
-        removeKey(name: name)
+	// MARK: - Private Properties
 
-        let access = getBioSecAccessControl()
-        let tag = name.data(using: .utf8)!
-        let attributes: [String: Any] = [
-            kSecAttrKeyType as String           : kSecAttrKeyTypeEC,
-            kSecAttrKeySizeInBits as String     : KeychainManager.bitSize,
-            kSecAttrTokenID as String           : kSecAttrTokenIDSecureEnclave,
-            kSecPrivateKeyAttrs as String : [
-                kSecAttrIsPermanent as String       : true,
-                kSecAttrApplicationTag as String    : tag,
-                kSecAttrAccessControl as String     : access
-            ] as [String : Any]
-        ]
-        
-        var error: Unmanaged<CFError>?
-        guard let privateKey = SecKeyCreateRandomKey(attributes as CFDictionary, &error) else {
-            throw error!.takeRetainedValue() as Error
-        }
-        
-        return privateKey
-    }
-    
-    private static func getBioSecAccessControl() -> SecAccessControl {
-        var access: SecAccessControl?
-        var error: Unmanaged<CFError>?
-        
-        access = SecAccessControlCreateWithFlags(nil,
-            kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
-            .userPresence,
-            &error)
-        precondition(access != nil, "SecAccessControlCreateWithFlags failed")
-        return access!
-    }
-    
-    // MARK: - Read key from keychain
-    
-    static func loadKey(name: String, context: LAContext) -> Result<SecKey, KeyManagmentError> {
-        let tag = name.data(using: .utf8)!
-        var query: [String: Any] = [
-            kSecClass as String                 : kSecClassKey,
-            kSecAttrApplicationTag as String    : tag,
-            kSecAttrKeyType as String           : kSecAttrKeyTypeEC,
-            kSecReturnRef as String             : true
-        ]
-        
-        query[kSecUseAuthenticationContext as String] = context
-        // Prevent system UI from automatically requesting Touc ID/Face ID authentication
-        // just in case someone passes here an LAContext instance without
-        // a prior evaluateAccessControl call
-        query[kSecUseAuthenticationUI as String] = kSecUseAuthenticationUISkip
-        
-        var item: CFTypeRef?
-        let status = SecItemCopyMatching(query as CFDictionary, &item)
-        guard status == errSecSuccess else {
-            return .failure(.failedToFetchKey)
-        }
-        return .success((item as! SecKey))
-    }
-    
-    // MARK: - Delete keys from Keychain
-    
-    static func removeKey(name: String) {
-        let tag = name.data(using: .utf8)!
-        let query: [String: Any] = [
-            kSecClass as String                 : kSecClassKey,
-            kSecAttrApplicationTag as String    : tag
-        ]
+	private static let bitSize = 256
+	private static var context: LAContext {
+		.init()
+	}
 
-        SecItemDelete(query as CFDictionary)
-    }
-    
-    static func remove(key: String) {
-        let query = [
-            kSecClass as String       : kSecClassGenericPassword as String,
-            kSecAttrAccount as String : key]
-        
-        SecItemDelete(query as CFDictionary)
-    }
-    
+	// MARK: - Create and store keys in Keychain
+
+	static func createPrivateKey(name: String) throws -> SecKey {
+		// First check if key already exists
+		if let key = keyExists(name: name) {
+			return key
+		}
+
+		let access = getBioSecAccessControl()
+		let tag = name.data(using: .utf8)!
+		let attributes: [String: Any] = [
+			kSecAttrKeyType as String: kSecAttrKeyTypeEC,
+			kSecAttrKeySizeInBits as String: KeychainManager.bitSize,
+			kSecAttrTokenID as String: kSecAttrTokenIDSecureEnclave,
+			kSecPrivateKeyAttrs as String: [
+				kSecAttrIsPermanent as String: true,
+				kSecAttrApplicationTag as String: tag,
+				kSecAttrAccessControl as String: access,
+			] as [String: Any],
+		]
+
+		var error: Unmanaged<CFError>?
+		guard let privateKey = SecKeyCreateRandomKey(attributes as CFDictionary, &error) else {
+			throw error!.takeRetainedValue() as Error
+		}
+
+		return privateKey
+	}
+
+	private static func getBioSecAccessControl() -> SecAccessControl {
+		var access: SecAccessControl?
+		var error: Unmanaged<CFError>?
+
+		access = SecAccessControlCreateWithFlags(
+			nil,
+			kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
+			[.privateKeyUsage, .userPresence],
+			&error
+		)
+		precondition(access != nil, "SecAccessControlCreateWithFlags failed")
+		return access!
+	}
+
+	// MARK: - Read key from keychain
+
+	static func loadKey(name: String, context: LAContext) -> Result<SecKey, KeyManagmentError> {
+		let tag = name.data(using: .utf8)!
+		var query: [String: Any] = [
+			kSecClass as String: kSecClassKey,
+			kSecAttrApplicationTag as String: tag,
+			kSecAttrKeyType as String: kSecAttrKeyTypeEC,
+			kSecReturnRef as String: true,
+		]
+
+		query[kSecUseAuthenticationContext as String] = context
+		// Prevent system UI from automatically requesting Touc ID/Face ID authentication
+		// just in case someone passes here an LAContext instance without
+		// a prior evaluateAccessControl call
+		query[kSecUseAuthenticationUI as String] = kSecUseAuthenticationUISkip
+
+		var item: CFTypeRef?
+		let status = SecItemCopyMatching(query as CFDictionary, &item)
+		guard status == errSecSuccess else {
+			return .failure(.privateKeyFetchFailed)
+		}
+		return .success(item as! SecKey)
+	}
+
+	static func keyExists(name: String) -> SecKey? {
+		switch loadKey(name: name, context: context) {
+		case let .success(key):
+			return key
+		case .failure:
+			return nil
+		}
+	}
+
+	// MARK: - Delete keys from Keychain
+
+	static func removeKey(name: String) {
+		let tag = name.data(using: .utf8)!
+		let query: [String: Any] = [
+			kSecClass as String: kSecClassKey,
+			kSecAttrApplicationTag as String: tag,
+		]
+
+		SecItemDelete(query as CFDictionary)
+	}
+
+	static func remove(key: String) {
+		let query = [
+			kSecClass as String: kSecClassGenericPassword as String,
+			kSecAttrAccount as String: key,
+		]
+
+		SecItemDelete(query as CFDictionary)
+	}
 }
--- Pino-iOS/Pino-iOS/Foundation/SecureEnclave/SecureEnclave.swift
+++ Pino-iOS/Pino-iOS/Foundation/SecureEnclave/SecureEnclave.swift
@@ -6,23 +6,93 @@
 //
 
 import Foundation
+import LocalAuthentication
 
 enum KeyManagmentError: LocalizedError {
-    case failedToFetchKey
+	case privateKeyFetchFailed
+	case publicKeyFetchFailed
+	case keyExists
+	case algorithmNotSupported
+	case encryptionFailed
+	case decryptionFailed
 }
 
-protocol KeyManagmentProtocol {
-    var algorithm: SecKeyAlgorithm { get }
-    func createPrivateKey(name: String) -> SecKey
-    func fetchPrivateKey(name: String) -> SecKey?
-}
-
-extension KeyManagmentProtocol {
-    var algorithm: SecKeyAlgorithm {
-        .eciesEncryptionCofactorVariableIVX963SHA256AESGCM
-    }
+public protocol KeyManagmentProtocol {
+	func encrypt(plainData: Data, withPublicKeyLabel label: String) -> Data
+	func decrypt(cipherData: Data, withPublicKeyLabel label: String) throws -> Data
 }
 
 class SecureEnclave: KeyManagmentProtocol {
-    
+	// MARK: - Private Properties
+
+	private var algorithm: SecKeyAlgorithm {
+		.eciesEncryptionCofactorVariableIVX963SHA256AESGCM
+	}
+
+	// MARK: - Public Methods
+
+	public func encrypt(plainData: Data, withPublicKeyLabel label: String) -> Data {
+		do {
+			// 1: Create private key (If private key exists it will be returned else created)
+			let privateKey = try KeychainManager.createPrivateKey(name: label)
+
+			// 2: Generate public key from our private key
+			guard let publicKey = SecKeyCopyPublicKey(privateKey) else {
+				fatalError(KeyManagmentError.publicKeyFetchFailed.localizedDescription)
+			}
+
+			// 3: Check if encryption algorithm is supported (Supported iOS 10.0+)
+			guard SecKeyIsAlgorithmSupported(publicKey, .encrypt, algorithm) else {
+				fatalError(KeyManagmentError.algorithmNotSupported.localizedDescription)
+			}
+
+			// 4: Start Encrypting plain data
+			var error: Unmanaged<CFError>?
+			let cipherTextData = SecKeyCreateEncryptedData(
+				publicKey,
+				algorithm,
+				plainData as CFData,
+				&error
+			) as Data?
+			guard let cipherData = cipherTextData else {
+				fatalError(KeyManagmentError.encryptionFailed.localizedDescription)
+			}
+
+			return cipherData
+
+		} catch {
+			fatalError(error.localizedDescription)
+		}
+	}
+
+	public func decrypt(cipherData: Data, withPublicKeyLabel label: String) throws -> Data {
+		// 1: Load private key
+		#warning("context should be analyzed more")
+		let fetchedPrivateKey = KeychainManager.loadKey(name: label, context: LAContext())
+		var privateKey: SecKey!
+
+		switch fetchedPrivateKey {
+		case let .success(key):
+			privateKey = key
+		case let .failure(error):
+			fatalError(error.localizedDescription)
+		}
+
+		// 2: Check if encryption algorithm is supported (Supported iOS 10.0+)
+		guard SecKeyIsAlgorithmSupported(privateKey, .decrypt, algorithm) else {
+			fatalError(KeyManagmentError.algorithmNotSupported.localizedDescription)
+		}
+
+		var error: Unmanaged<CFError>?
+		let clearTextData = SecKeyCreateDecryptedData(
+			privateKey,
+			algorithm,
+			cipherData as CFData,
+			&error
+		) as Data?
+		guard let decryptedData = clearTextData else {
+			fatalError(KeyManagmentError.decryptionFailed.localizedDescription)
+		}
+		return decryptedData
+	}
 }
