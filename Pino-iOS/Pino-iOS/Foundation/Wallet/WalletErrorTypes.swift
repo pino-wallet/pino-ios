2a9aefe5c76aecf2be9b4dbf9e327b1f7106df6e
sobhaneskandari <eskandari.sobhan@gmail.com>
2023-05-21 19:17:56 +0330

    feat: add descriptions to wallet errors

--- Pino-iOS/Pino-iOS/Foundation/Wallet/WalletErrorTypes.swift
+++ Pino-iOS/Pino-iOS/Foundation/Wallet/WalletErrorTypes.swift
@@ -17,14 +17,46 @@ public enum WalletError: LocalizedError {
 	case accountNotFound
 	case accountDeletionFailed
 	case unknownError
+    
+    public var errorDescription: String? {
+        switch self {
+        case .mnemonicGenerationFailed:
+            return "Failed to create mnemonics"
+        case .walletCreationFailed:
+            return "Failed to create wallet"
+        case .importAccountFailed:
+            return "Failed to import account"
+        case .accountAlreadyExists:
+            return "Account already exists"
+        case .accountNotFound:
+            return "Account could not be found"
+        case .accountDeletionFailed:
+            return "Failed to delete account"
+        case .unknownError:
+            return "Unknown Error"
+        }
+    }
 }
 
 public enum WalletValidatorError: LocalizedError {
 	case privateKeyIsInvalid
 	case publicKeyIsInvalid
 	case addressIsInvalid
-	case seedIsInvalid
 	case mnemonicIsInvalid
+    
+    public var errorDescription: String? {
+        switch self {
+        
+        case .privateKeyIsInvalid:
+            return "Private Key is invalid"
+        case .publicKeyIsInvalid:
+            return "Public Key is invalid"
+        case .addressIsInvalid:
+            return "Address is invalid"
+        case .mnemonicIsInvalid:
+            return "Mnemonics is invalid"
+        }
+    }
 }
 
 public enum KeyManagementError: LocalizedError {
@@ -34,6 +66,24 @@ public enum KeyManagementError: LocalizedError {
 	case mnemonicsRetrievalFailed
 	case privateKeyRetrievalFailed
 	case publicKeyRetrievalFailed
+    
+    public var errorDescription: String? {
+        switch self {
+        
+        case .mnemonicsStorageFailed:
+            return "Failed to store mnemonics"
+        case .privateKeyStorageFailed:
+            return "Failed to store private key"
+        case .publicKeyStorageFailed:
+            return "Failed to store public key"
+        case .mnemonicsRetrievalFailed:
+            return "Failed to fetch mnemonics"
+        case .privateKeyRetrievalFailed:
+            return "Failed to fetch private key"
+        case .publicKeyRetrievalFailed:
+            return "Failed to fetch public key"
+        }
+    }
 }
 
 public enum WalletOperationError: LocalizedError {
