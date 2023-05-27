bb20277cba4fb7cfca0b3d4295fc8273b04c7774
Sobhan Eskandari <eskandari.sobhan@gmail.com>
2023-05-27 18:17:53 +0330

    feat: send balance request each 12 secons

--- Pino-iOS/Pino-iOS/Flows/Home/ViewControllers/HomepageViewController.swift
+++ Pino-iOS/Pino-iOS/Flows/Home/ViewControllers/HomepageViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class HomepageViewController: UIViewController {
 	// MARK: - Private Properties
 
-	private let homeVM = HomepageViewModel()
+    private var homeVM:HomepageViewModel!
 	private var profileVM: ProfileViewModel!
 	private var cancellables = Set<AnyCancellable>()
 	private var assetsCollectionView: AssetsCollectionView!
@@ -29,7 +29,12 @@ class HomepageViewController: UIViewController {
 	}
 
 	override func loadView() {
+        homeVM = HomepageViewModel(completion: { error in
+            #warning("A toast should be sgown in case of error")
+            print(error)
+        })
 		profileVM = ProfileViewModel(walletInfo: homeVM.walletInfo)
+        
 		setupView()
 		setupNavigationBar()
 		setupBindings()
--- Pino-iOS/Pino-iOS/Flows/Home/ViewModel/HomepageViewModel/HomepageViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/Home/ViewModel/HomepageViewModel/HomepageViewModel.swift
@@ -12,107 +12,118 @@ import Hyperconnectivity
 import Network
 
 class HomepageViewModel {
-	// MARK: - Public Properties
+    // MARK: - Public Properties
+    
+    @Published
+    public var walletInfo: AccountInfoViewModel!
+    @Published
+    public var walletBalance: WalletBalanceViewModel?
+    @Published
+    public var assetsList: [AssetViewModel]?
+    @Published
+    public var positionAssetsList: [AssetViewModel]?
+    @Published
+    public var securityMode = false
+    @Published
+    public var manageAssetsList: [AssetViewModel]?
+    public var selectedAssets = [SelectedAsset]()
+    @Published
+    public var assetsModelList: [AssetProtocol]!
+    
+    public let copyToastMessage = "Copied!"
+    public let sendButtonTitle = "Send"
+    public let receiveButtonTitle = "Receive"
+    public let sendButtonImage = "arrow_up"
+    public let receiveButtonImage = "arrow_down"
+    public let showBalanceButtonTitle = "Show balance"
+    public let showBalanceButtonImage = "eye"
+    
+    // MARK: Internal Properties
+    
+    internal var internetConnectivity = InternetConnectivity()
+    internal var cancellables = Set<AnyCancellable>()
+    internal var walletAPIClient = WalletAPIMockClient()
+    internal var accountingAPIClient = AccountingAPIClient()
+    
+    let coreDataStack = CoreDataStack.pinoSharedStack
+    let managedContext = CoreDataStack.pinoSharedStack.managedContext
+    
+    var subscriptions = Set<AnyCancellable>()
+    let start = Date()
 
-	@Published
-	public var walletInfo: AccountInfoViewModel!
-	@Published
-	public var walletBalance: WalletBalanceViewModel?
-	@Published
-	public var assetsList: [AssetViewModel]?
-	@Published
-	public var positionAssetsList: [AssetViewModel]?
-	@Published
-	public var securityMode = false
-	@Published
-	public var manageAssetsList: [AssetViewModel]?
-	public var selectedAssets = [SelectedAsset]()
-	@Published
-	public var assetsModelList: [AssetProtocol]!
-
-	public let copyToastMessage = "Copied!"
-	public let sendButtonTitle = "Send"
-	public let receiveButtonTitle = "Receive"
-	public let sendButtonImage = "arrow_up"
-	public let receiveButtonImage = "arrow_down"
-	public let showBalanceButtonTitle = "Show balance"
-	public let showBalanceButtonImage = "eye"
-
-	// MARK: Internal Properties
-
-	internal var internetConnectivity = InternetConnectivity()
-
-	internal var cancellables = Set<AnyCancellable>()
-
-	internal var walletAPIClient = WalletAPIMockClient()
-	internal var accountingAPIClient = AccountingAPIClient()
-
-	let coreDataStack = CoreDataStack.pinoSharedStack
-	let managedContext = CoreDataStack.pinoSharedStack.managedContext
-
-	// MARK: - Initializers
-
-	init() {
-		getSelectedAssetsFromCoreData()
-		getWalletInfo()
-		setupBindings()
-	}
-
-	// MARK: - Public Methods
-
-	public func getHomeData(completion: @escaping (HomeNetworkError?) -> Void) {
-		internetConnectivity.$isConnected.tryCompactMap { $0 }.sink { _ in
-		} receiveValue: { isConnected in
-			if isConnected {
-				self.getAssetsList { result in
-					switch result {
-					case let .success(assets):
-						self.getManageAsset(assets: assets)
-						completion(nil)
-					case .failure:
-						completion(.requestFailed)
-					}
-				}
-			} else {
-				completion(.networkConnection)
-			}
-		}.store(in: &cancellables)
-	}
-
-	// MARK: Private Methods
-
-	private func switchSecurityMode(_ isOn: Bool) {
-		if let walletBalance {
-			walletBalance.switchSecurityMode(isOn)
-		}
-		if let assetsList {
-			for asset in assetsList {
-				asset.switchSecurityMode(isOn)
-			}
-		}
-		if let positionAssetsList {
-			for asset in positionAssetsList {
-				asset.switchSecurityMode(isOn)
-			}
-		}
-		assetsList = assetsList
-		positionAssetsList = positionAssetsList
-	}
-
-	private func setupBindings() {
-		$securityMode.sink { [weak self] securityMode in
-			guard let self = self else { return }
-			self.switchSecurityMode(securityMode)
-		}.store(in: &cancellables)
-
-		$manageAssetsList.sink { assets in
-			guard let assets else { return }
-			self.assetsList = assets.filter { $0.isSelected }
-		}.store(in: &cancellables)
-
-		$assetsList.sink { assets in
-			guard let assets else { return }
-			self.getWalletBalance(assets: assets)
-		}.store(in: &cancellables)
-	}
+    // MARK: - Initializers
+    
+    init(completion: @escaping (HomeNetworkError?) -> Void) {
+        getHomeDataWithTimer(completion: completion)
+        getSelectedAssetsFromCoreData()
+        getWalletInfo()
+        setupBindings()
+    }
+    
+    // MARK: - Public Methods
+    func getHomeDataWithTimer(completion: @escaping (HomeNetworkError?) -> Void) {
+        Timer.publish(every: 12.0, on: .main, in: .common)
+            .autoconnect()
+            .sink { (seconds) in
+                print("Seconds: \(seconds)")
+                self.getHomeData(completion: completion)
+            }
+            .store(in: &subscriptions)
+    }
+    
+    public func getHomeData(completion: @escaping (HomeNetworkError?) -> Void) {
+        internetConnectivity.$isConnected.tryCompactMap { $0 }.sink { _ in
+        } receiveValue: { isConnected in
+            if isConnected {
+                self.getAssetsList { result in
+                    switch result {
+                    case let .success(assets):
+                        self.getManageAsset(assets: assets)
+                        completion(nil)
+                    case .failure:
+                        completion(.requestFailed)
+                    }
+                }
+            } else {
+                completion(.networkConnection)
+            }
+        }.store(in: &cancellables)
+    }
+    
+    // MARK: Private Methods
+    
+    private func switchSecurityMode(_ isOn: Bool) {
+        if let walletBalance {
+            walletBalance.switchSecurityMode(isOn)
+        }
+        if let assetsList {
+            for asset in assetsList {
+                asset.switchSecurityMode(isOn)
+            }
+        }
+        if let positionAssetsList {
+            for asset in positionAssetsList {
+                asset.switchSecurityMode(isOn)
+            }
+        }
+        assetsList = assetsList
+        positionAssetsList = positionAssetsList
+    }
+    
+    private func setupBindings() {
+        $securityMode.sink { [weak self] securityMode in
+            guard let self = self else { return }
+            self.switchSecurityMode(securityMode)
+        }.store(in: &cancellables)
+        
+        $manageAssetsList.sink { assets in
+            guard let assets else { return }
+            self.assetsList = assets.filter { $0.isSelected }
+        }.store(in: &cancellables)
+        
+        $assetsList.sink { assets in
+            guard let assets else { return }
+            self.getWalletBalance(assets: assets)
+        }.store(in: &cancellables)
+    }
 }
