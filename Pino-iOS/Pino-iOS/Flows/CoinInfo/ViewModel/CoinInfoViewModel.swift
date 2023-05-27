fda77baf96634a1f7132d8a6a3457023fa4a94de
Amirrk79 <amirmntmntmntmnt@gmail.com>
2023-05-27 15:38:59 +0330

    feat: copy coin website and contract address on tap in coininfo

--- Pino-iOS/Pino-iOS/Flows/CoinInfo/ViewModel/CoinInfoViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/CoinInfo/ViewModel/CoinInfoViewModel.swift
@@ -40,6 +40,10 @@ class CoinInfoViewModel {
 	public let noAssetPriceText = "-"
 	public let unavailableRecentHistoryText = "The history are only available for verified assets!"
 	public let unavailableRecentHistoryIconName = "gray_error_alert"
+    public let copyWebsiteToastText = "Coin website has been copied"
+        public let copyContractAddressToastText = "Coin contract address has been copied"
+
+
 
 	#warning("this text is for testing and should be removed")
 	public let positionAssetInfoText = """
--- Pino-iOS/Pino-iOS/Flows/CoinInfo/ViewModel/CoinPortfolioViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/CoinInfo/ViewModel/CoinPortfolioViewModel.swift
@@ -60,13 +60,21 @@ struct CoinPortfolioViewModel {
 		let totalAmountInDollar = userAmount * coinPrice
 		return "$\(totalAmountInDollar.formattedAmountOf(type: .hold))"
 	}
+    
+    public var isEthCoin: Bool {
+            if coinPortfolioModel.detail!.id == AccountingEndpoint.ethID {
+                return true
+            }
+            return false
+        }
 
-	public var contractAddress: String {
-		if coinPortfolioModel.detail!.id == AccountingEndpoint.ethID {
-			return "-"
-		}
-		return coinPortfolioModel.detail!.id
-	}
+    public var contractAddress: String {
+        if isEthCoin {
+            return "-"
+        } else {
+            return coinPortfolioModel.detail!.id
+        }
+    }
 }
 
 extension CoinPortfolioViewModel {
