8f11d8f2074efa4b320d272f977ef9245318ab3f
Sobhan Eskandari <eskandari.sobhan@gmail.com>
2023-06-15 14:49:22 +0330

    fix: remove extra code abi code

--- Pino-iOS/Pino-iOS/Flows/Home/ViewModel/CustomAsset/AddCustomAssetViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/Home/ViewModel/CustomAsset/AddCustomAssetViewModel.swift
@@ -78,9 +78,6 @@ class AddCustomAssetViewModel {
 
 	// MARK: - Private Properties
 
-	private let erc20AbiString = """
-	[{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
-	"""
 	private var getCustomAssetInfoRequestWork: DispatchWorkItem!
 	private var readNodeContractKey = "0"
 	private var userAddressStaticCode = "0x"
