ac87d3b62c5430873a96885ef9d4b02875fb32b6
Amirrk79 <amirmntmntmntmnt@gmail.com>
2023-07-09 20:32:33 +0330

    fix: remove coin info vm changes because i need to do it on another branch

--- Pino-iOS/Pino-iOS/Flows/CoinInfo/ViewModel/CoinInfoViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/CoinInfo/ViewModel/CoinInfoViewModel.swift
@@ -10,89 +10,93 @@ import Foundation
 import Network
 
 class CoinInfoViewModel {
-	// MARK: - Public Properties
+    // MARK: - Public Properties
 
-	public let homeVM: HomepageViewModel
-	public let selectedAsset: AssetViewModel
+    public let homeVM: HomepageViewModel
+    public let selectedAsset: AssetViewModel
 
-	@Published
-	public var coinPortfolio: CoinPortfolioViewModel!
-	@Published
-	public var coinHistoryList: [ActivityCellViewModel]? = nil
+    @Published
+    public var coinPortfolio: CoinPortfolioViewModel!
+    @Published
+    public var coinHistoryList: [ActivityCellViewModel]!
 
-	public let requestFailedErrorToastMessage = "Couldn't refresh coin data"
-	public let connectionErrorToastMessage = "No internet connection"
-	public let infoActionSheetTitle = "Price impact"
-	public let infoActionSheetDescription =
-		"The difference between the market price and the estimated price based on your order size."
-	public let websiteTitle = "Website"
-	public let priceTitle = "Price"
-	public let contractAddressTitle = "Contract address"
-	public let protocolTitle = "Protocol"
-	public let positionTitle = "Position"
-	public let assetTitle = "Asset"
-	public let tooltipIconName = "info"
-	public let positionAssetTitle = "Position asset"
-	public let priceSepratorText = "|"
-	public let noUserAmountInDollarText = "--"
-	public let unverifiedAssetIcon = "unverified_asset"
-	public let noAssetPriceText = "-"
-	public let unavailableRecentHistoryText = "The history are only available for verified assets!"
-	public let unavailableRecentHistoryIconName = "gray_error_alert"
-	public let copyWebsiteToastText = "Coin website has been copied"
-	public let copyContractAddressToastText = "Coin contract address has been copied"
+    public let requestFailedErrorToastMessage = "Couldn't refresh coin data"
+    public let connectionErrorToastMessage = "No internet connection"
+    public let infoActionSheetTitle = "Price impact"
+    public let infoActionSheetDescription =
+        "The difference between the market price and the estimated price based on your order size."
+    public let websiteTitle = "Website"
+    public let priceTitle = "Price"
+    public let contractAddressTitle = "Contract address"
+    public let protocolTitle = "Protocol"
+    public let positionTitle = "Position"
+    public let assetTitle = "Asset"
+    public let tooltipIconName = "info"
+    public let positionAssetTitle = "Position asset"
+    public let priceSepratorText = "|"
+    public let noUserAmountInDollarText = "--"
+    public let unverifiedAssetIcon = "unverified_asset"
+    public let noAssetPriceText = "-"
+    public let unavailableRecentHistoryText = "The history are only available for verified assets!"
+    public let unavailableRecentHistoryIconName = "gray_error_alert"
+    public let copyWebsiteToastText = "Coin website has been copied"
+    public let copyContractAddressToastText = "Coin contract address has been copied"
 
-	#warning("this text is for testing and should be removed")
-	public let positionAssetInfoText = """
-	This asset represents your DAI collateral
-	 position in the Compound Protocol. Note that
-	 transferring this asset to another address will
-	 fully transfer your position to the new
-	 address.
-	"""
+    #warning("this text is for testing and should be removed")
+    public let positionAssetInfoText = """
+    This asset represents your DAI collateral
+     position in the Compound Protocol. Note that
+     transferring this asset to another address will
+     fully transfer your position to the new
+     address.
+    """
 
-	// MARK: - Private Properties
+    // MARK: - Private Properties
 
-	private var activityAPIClient = ActivityAPIClient()
-	private var walletManager = PinoWalletManager()
-	private var cancellables = Set<AnyCancellable>()
+    private var assetsAPIClient = AssetsAPIMockClient()
+    private var cancellables = Set<AnyCancellable>()
 
-	// MARK: - Inintializers
+    // MARK: - Inintializers
 
-	init(homeVM: HomepageViewModel, selectedAsset: AssetViewModel) {
-		self.homeVM = homeVM
-		self.selectedAsset = selectedAsset
-		getCoinPortfolio()
-		getHistoryList()
-	}
+    init(homeVM: HomepageViewModel, selectedAsset: AssetViewModel) {
+        self.homeVM = homeVM
+        self.selectedAsset = selectedAsset
+        getCoinPortfolio()
+        getHistoryList()
+    }
 
-	// MARK: - public Methods
+    // MARK: - public Methods
 
-	public func refreshCoinInfoData(completion: @escaping (APIError?) -> Void) {
-		getHistoryList()
-		GlobalVariables.shared.fetchSharedInfo().catch { error in
-			completion(APIError.unreachable)
-		}
-	}
+    public func refreshCoinInfoData(completion: @escaping (APIError?) -> Void) {
+        getHistoryList()
+        GlobalVariables.shared.fetchSharedInfo().catch { error in
+            completion(APIError.unreachable)
+        }
+    }
 
-	// MARK: - private Methods
+    // MARK: - private Methods
 
-	private func getCoinPortfolio() {
-		coinPortfolio = CoinPortfolioViewModel(coinPortfolioModel: selectedAsset.assetModel)
-	}
+    private func getCoinPortfolio() {
+        coinPortfolio = CoinPortfolioViewModel(coinPortfolioModel: selectedAsset.assetModel)
+    }
 
-	private func getHistoryList() {
-		let userAddress = walletManager.currentAccount.eip55Address
-		activityAPIClient.tokenActivities(userAddress: userAddress, tokenAddress: selectedAsset.id).sink { completed in
-			switch completed {
-			case .finished:
-				print("Coin activities received successfully")
-			case let .failure(error):
-				print(error)
-			}
-		} receiveValue: { [weak self] activities in
-			self?.coinHistoryList = activities
-				.compactMap { ActivityCellViewModel(activityModel: $0, currentAddress: userAddress) }
-		}.store(in: &cancellables)
-	}
+    private func getHistoryList() {
+        assetsAPIClient.coinHistory().sink { completed in
+            switch completed {
+            case .finished:
+                print("Coin history received successfully")
+            case let .failure(error):
+                print(error)
+            }
+        } receiveValue: { [weak self] coinHistoryModelList in
+            let walletManager = PinoWalletManager()
+            self?.coinHistoryList = coinHistoryModelList
+                .compactMap {
+                    ActivityCellViewModel(activityModel: $0, currentAddress: walletManager.currentAccount.eip55Address)
+                }
+            #warning(
+                "this line is for testing because these two publishers were pinned to each other and their value should be changed together"
+            )
+        }.store(in: &cancellables)
+    }
 }
