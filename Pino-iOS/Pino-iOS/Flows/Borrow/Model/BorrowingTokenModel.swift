398cd7e9af85d611a6c59d7aedd6ee138ab0c101
Amirrk79 <amirmntmntmntmnt@gmail.com>
2023-08-26 11:46:23 +0330

    fix: resolve conflicts

---
+++ Pino-iOS/Pino-iOS/Flows/Borrow/Model/BorrowingTokenModel.swift
@@ -0,0 +1,15 @@
+//
+//  BorrowingTokenModel.swift
+//  Pino-iOS
+//
+//  Created by Amir hossein kazemi seresht on 8/23/23.
+//
+
+struct BorrowingTokenModel {
+    // MARK: - Public Properties
+
+    public var tokenImage: String
+    #warning("this values should be BigNumber")
+    public var tokenSharedBorrowingPercentage: Double
+    public var prevTokenSharedBorrowingPercentage: Double
+}
