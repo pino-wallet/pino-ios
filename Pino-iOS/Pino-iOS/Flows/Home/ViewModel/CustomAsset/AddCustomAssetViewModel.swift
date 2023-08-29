fd3238d284c04f14b114e83803d55c2819432d8a
Amirrk79 <amirmntmntmntmnt@gmail.com>
2023-08-29 10:54:28 +0330

    style: format the code

--- Pino-iOS/Pino-iOS/Flows/Home/ViewModel/CustomAsset/AddCustomAssetViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/Home/ViewModel/CustomAsset/AddCustomAssetViewModel.swift
@@ -128,15 +128,15 @@ class AddCustomAssetViewModel {
 				return
 			} else {
 				changeViewStatusClosure(.pending)
-                let contractChecksumAddress = Web3Core.shared.getChecksumOfEip55Address(eip55Address: textFieldText)
-                Web3Core.shared.getCustomAssetInfo(contractAddress: contractChecksumAddress)
+				let contractChecksumAddress = Web3Core.shared.getChecksumOfEip55Address(eip55Address: textFieldText)
+				Web3Core.shared.getCustomAssetInfo(contractAddress: contractChecksumAddress)
 					.done { [weak self] assetInfo in
 						if let name = assetInfo[.name]?.description,
 						   let symbol = assetInfo[.symbol]?.description,
 						   let balance = assetInfo[.balance]?.description,
 						   let decimal = assetInfo[.decimal]?.description {
 							self?.customAssetVM = CustomAssetViewModel(customAsset: CustomAssetModel(
-                                id: contractChecksumAddress.trimmingCharacters(in: .whitespaces),
+								id: contractChecksumAddress.trimmingCharacters(in: .whitespaces),
 								name: name,
 								symbol: symbol,
 								balance: balance,
--- Pino-iOS/Pino-iOS/Foundation/Web3/Web3Core.swift
+++ Pino-iOS/Pino-iOS/Foundation/Web3/Web3Core.swift
@@ -52,16 +52,15 @@ public class Web3Core {
 	public static var shared = Web3Core()
 
 	// MARK: - Public Methods
-    
-    public func getChecksumOfEip55Address(eip55Address: String) -> String {
-        do {
-            let ethAddress = try EthereumAddress(hex: eip55Address, eip55: false)
-            return ethAddress.hex(eip55: true)
-        } catch {
-            fatalError("Cannot get checksum of wrong eip55 address")
-        }
-        
-    }
+
+	public func getChecksumOfEip55Address(eip55Address: String) -> String {
+		do {
+			let ethAddress = try EthereumAddress(hex: eip55Address, eip55: false)
+			return ethAddress.hex(eip55: true)
+		} catch {
+			fatalError("Cannot get checksum of wrong eip55 address")
+		}
+	}
 
 	public func getAllowanceOf(
 		contractAddress: String,
