3c8a4524bdbfd1b9637a6d38013041212b24ae60
Amirrk79 <amirmntmntmntmnt@gmail.com>
2023-08-29 14:19:17 +0330

    feat: PinoRightSideImageButton and used it where was needed

---
+++ Pino-iOS/Pino-iOS/DesignSystem/Button/PinoRightSideImageButton/PinoRightSideImageStyle.swift
@@ -0,0 +1,30 @@
+//
+//  PinoRightSideImageStyle.swift
+//  Pino-iOS
+//
+//  Created by Amir hossein kazemi seresht on 8/29/23.
+//
+
+import UIKit
+
+extension PinoRightSideImageButton {
+    // Specifies a visual theme of the button
+    public struct Style {
+        public let titleColor: UIColor
+        public let backgroundColor: UIColor
+        public let borderColor: UIColor?
+    }
+}
+
+extension PinoRightSideImageButton.Style {
+    public static let primary = PinoRightSideImageButton.Style(
+        titleColor: .Pino.white,
+        backgroundColor: .Pino.primary,
+        borderColor: .clear
+    )
+    public static let clear = PinoRightSideImageButton.Style(
+        titleColor: .Pino.primary,
+        backgroundColor: .Pino.clear,
+        borderColor: .clear
+    )
+}
