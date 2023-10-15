235354c162062743ff1fe0b5aeef9081bea8ece7
Amirrk79 <amirmntmntmntmnt@gmail.com>
2023-10-15 17:22:08 +0330

    feat: add loading approve changes and states

--- Pino-iOS/Pino-iOS/DesignSystem/Loading/PinoLoading.swift
+++ Pino-iOS/Pino-iOS/DesignSystem/Loading/PinoLoading.swift
@@ -8,72 +8,71 @@
 import UIKit
 
 class PinoLoading: UIView {
-	// MARK: - Public Properties
-
-	public var size: CGFloat {
-		didSet {
-			setupContraints()
-		}
-	}
-
-	public var loadingSpeed: SpeedType {
-		didSet {
-			removeAnimation()
-			startLoadingAnimation(duration: loadingSpeed.rawValue)
-		}
-	}
-
-	public var imageType: ImageType {
-		didSet {
-			loadingImageView.image = UIImage(named: imageType.rawValue)
-		}
-	}
-
-	// MARK: - Private Properties
-
-	private let loadingImageView = UIImageView()
-
-	// MARK: - Initializers
-
-	init(size: CGFloat, imageType: ImageType = .primary, loadingSpeed: SpeedType = .normal) {
-		self.size = size
-		self.imageType = imageType
-		self.loadingSpeed = loadingSpeed
-		super.init(frame: .zero)
-		setupView()
-		setupStyles()
-		setupContraints()
-		startLoadingAnimation(duration: loadingSpeed.rawValue)
-	}
-
-	required init(coder: NSCoder) {
-		fatalError("init(coder:) has not been implemented")
-	}
-
-	// MARK: - Private Methods
-
-	private func setupView() {
-		addSubview(loadingImageView)
-	}
-
-	private func setupStyles() {
-		loadingImageView.image = UIImage(named: imageType.rawValue)
-	}
-
-	private func removeAnimation() {
-		layer.removeAllAnimations()
-	}
-
-	private func startLoadingAnimation(duration: Double) {
-		UIView.animate(withDuration: duration, delay: 0.0, options: .curveLinear, animations: { [weak self] in
-			guard let self else {
-				return
-			}
-			self.loadingImageView.transform = self.loadingImageView.transform.rotated(by: .pi)
-		}) { _ in
-			self.startLoadingAnimation(duration: duration)
-		}
-	}
+    // MARK: - Public Properties
+    
+    public var size: CGFloat {
+        didSet {
+            setupContraints()
+        }
+    }
+    
+    public var loadingSpeed: SpeedType {
+        didSet {
+            loadingAnimator.stopAnimation(true)
+            startLoadingAnimation(duration: loadingSpeed.rawValue)
+        }
+    }
+    
+    public var imageType: ImageType {
+        didSet {
+            loadingImageView.image = UIImage(named: imageType.rawValue)
+        }
+    }
+    
+    // MARK: - Private Properties
+    
+    private let loadingImageView = UIImageView()
+    private var loadingAnimator: UIViewPropertyAnimator!
+    
+    // MARK: - Initializers
+    
+    init(size: CGFloat, imageType: ImageType = .primary, loadingSpeed: SpeedType = .normal) {
+        self.size = size
+        self.imageType = imageType
+        self.loadingSpeed = loadingSpeed
+        super.init(frame: .zero)
+        setupView()
+        setupStyles()
+        setupContraints()
+        startLoadingAnimation(duration: loadingSpeed.rawValue)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupView() {
+        addSubview(loadingImageView)
+    }
+    
+    private func setupStyles() {
+        loadingImageView.image = UIImage(named: imageType.rawValue)
+    }
+    
+    private func startLoadingAnimation(duration: Double) {
+        loadingAnimator =  UIViewPropertyAnimator(duration: duration, curve: .linear, animations: { [weak self] in
+            guard let self else {
+                return
+            }
+              self.loadingImageView.transform = self.loadingImageView.transform.rotated(by: .pi)
+          })
+        loadingAnimator.addCompletion { [weak self] _ in
+            self?.startLoadingAnimation(duration: duration)
+        }
+        loadingAnimator.startAnimation()
+    }
 
 	private func setupContraints() {
 		loadingImageView.pin(.allEdges(padding: 0), .fixedWidth(size), .fixedHeight(size))
--- Pino-iOS/Pino-iOS/Flows/Swap/View/Approve/ApprovingLoadingView.swift
+++ Pino-iOS/Pino-iOS/Flows/Swap/View/Approve/ApprovingLoadingView.swift
@@ -7,14 +7,27 @@
 
 import Foundation
 import UIKit
+import Combine
 
 class ApprovingLoadingView: UIView {
 	// MARK: Private Properties
 
 	private let approvngContractLoadingVM: ApprovingLoadingViewModel!
+    private let clearNavigationBar = ClearNavigationBar()
+    private let clearNavigationBarRightSideView = UIView()
+    private let navigationBarDismissButton = UIButton()
+    private let warningTitleImageView = UIImageView()
+    private let tryAgainButton = PinoButton(style: .active)
+    private let speedUpDescriptionContainerView = UIView()
+    private let speedUpDescriptionStackView = UIStackView()
+    private let speedUpDescriptionAlertImageView = UIImageView()
+    private let speedUpDescriptionLabel = PinoLabel(style: .description, text: "")
 	private let loading = PinoLoading(size: 50)
-	private let loadingTextLabel = PinoLabel(style: .description, text: "")
+	private let loadingTextLabel = PinoLabel(style: .title, text: "")
+    private let loadingDescriptionLabel = PinoLabel(style: .info, text: "")
+    private var speedUpButton: PinoRightSideImageButton!
 	private let loadingStackView = UIStackView()
+    private var cancellables = Set<AnyCancellable>()
 
 	// MARK: Initializers
 
@@ -25,6 +38,8 @@ class ApprovingLoadingView: UIView {
 		setupView()
 		setupStyle()
 		setupContstraint()
+        setupBindings()
+        updateViewWithStatus(status: approvingLoadingVM.approveLoadingStatus)
 	}
 
 	required init?(coder: NSCoder) {
@@ -34,31 +49,144 @@ class ApprovingLoadingView: UIView {
 	// MARK: - Private Methods
 
 	private func setupView() {
+        speedUpButton = PinoRightSideImageButton(imageName: approvngContractLoadingVM.speedUpImageName, style: .primary)
+        
 		loadingStackView.addSubview(loading)
 
+        loadingStackView.addArrangedSubview(warningTitleImageView)
 		loadingStackView.addArrangedSubview(loading)
 		loadingStackView.addArrangedSubview(loadingTextLabel)
+        loadingStackView.addArrangedSubview(loadingDescriptionLabel)
+        
+        speedUpDescriptionStackView.addArrangedSubview(speedUpDescriptionAlertImageView)
+        speedUpDescriptionStackView.addArrangedSubview(speedUpDescriptionLabel)
+        
+        speedUpDescriptionContainerView.addSubview(speedUpDescriptionStackView)
+        
+        clearNavigationBarRightSideView.addSubview(navigationBarDismissButton)
+        
+        clearNavigationBar.setRightSectionView(view: clearNavigationBarRightSideView)
 
+        addSubview(clearNavigationBar)
 		addSubview(loadingStackView)
+        addSubview(speedUpDescriptionContainerView)
+        addSubview(tryAgainButton)
+        addSubview(speedUpButton)
 	}
 
 	private func setupStyle() {
-		backgroundColor = .Pino.background
+        speedUpButton.title = approvngContractLoadingVM.speedUpButtonText
+        
+        tryAgainButton.title = approvngContractLoadingVM.tryAgainButtonText
+        
+        warningTitleImageView.image = UIImage(named: approvngContractLoadingVM.warningImageName)
+        
+        speedUpDescriptionAlertImageView.image = UIImage(named: approvngContractLoadingVM.grayErrorAlertImageName)
+        
+        navigationBarDismissButton.setImage(UIImage(named: approvngContractLoadingVM.dismissButtonImageName), for: .normal)
+        
+		backgroundColor = .Pino.secondaryBackground
 
-		loadingTextLabel.font = .PinoStyle.mediumTitle2
-		loadingTextLabel.text = "Approving..."
-		loadingTextLabel.numberOfLines = 0
-		loadingTextLabel.textAlignment = .center
+		loadingTextLabel.font = .PinoStyle.semiboldTitle2
+        
 
 		loadingStackView.axis = .vertical
 		loadingStackView.spacing = 24
 		loadingStackView.alignment = .center
+        loadingStackView.setCustomSpacing(8, after: loadingTextLabel)
+        
+        speedUpDescriptionStackView.axis = .horizontal
+        speedUpDescriptionStackView.spacing = 4
+        speedUpDescriptionStackView.alignment = .top
+        
+        loadingDescriptionLabel.textColor = .Pino.secondaryLabel
+        
+        
+        speedUpDescriptionContainerView.backgroundColor = .Pino.webBackground
+        speedUpDescriptionContainerView.layer.cornerRadius = 8
+        
+        speedUpDescriptionLabel.textColor = .Pino.label
+        speedUpDescriptionLabel.text = approvngContractLoadingVM.speedUpDescriptionText
 	}
+    
+    private func setupBindings() {
+        approvngContractLoadingVM.$approveLoadingStatus.sink { approveLoadingStatus in
+            self.updateViewWithStatus(status: approveLoadingStatus)
+        }.store(in: &cancellables)
+    }
+    
+    private func updateViewWithStatus(status: ApprovingLoadingViewModel.ApproveLoadingStatuses) {
+        switch status {
+        case .normalLoading:
+            loading.isHidden = false
+            loading.loadingSpeed = .normal
+            loading.imageType = .primary
+            warningTitleImageView.isHidden = true
+            speedUpButton.isHidden = true
+            speedUpDescriptionContainerView.isHidden = true
+            tryAgainButton.isHidden = true
+            navigationBarDismissButton.isHidden = true
+            setLoadingTextLabel(text: approvngContractLoadingVM.approvingText)
+            setLoadingDescriptionLabelText(text: approvngContractLoadingVM.takeFewSecondsText)
+        case .showSpeedUp:
+            loading.isHidden = false
+            loading.loadingSpeed = .normal
+            loading.imageType = .primary
+            warningTitleImageView.isHidden = true
+            navigationBarDismissButton.isHidden = false
+            tryAgainButton.isHidden = true
+            speedUpDescriptionContainerView.isHidden = false
+            speedUpButton.isHidden = false
+            setLoadingTextLabel(text: approvngContractLoadingVM.approvingText)
+            setLoadingDescriptionLabelText(text: approvngContractLoadingVM.takeFewMinutesText)
+        case .fastLoading:
+            warningTitleImageView.isHidden = true
+            loading.isHidden = false
+            loading.loadingSpeed = .fast
+            loading.imageType = .rainbow
+            tryAgainButton.isHidden = true
+            speedUpButton.isHidden = true
+            navigationBarDismissButton.isHidden = true
+            speedUpDescriptionContainerView.isHidden = true
+            setLoadingTextLabel(text: approvngContractLoadingVM.approvingText)
+            setLoadingDescriptionLabelText(text: approvngContractLoadingVM.takeFewSecondsText)
+        case .error:
+            loading.isHidden = true
+            warningTitleImageView.isHidden = false
+            tryAgainButton.isHidden = false
+            speedUpButton.isHidden = true
+            speedUpDescriptionContainerView.isHidden = true
+            setLoadingTextLabel(text: approvngContractLoadingVM.somethingWentWeongText)
+            setLoadingDescriptionLabelText(text: approvngContractLoadingVM.tryAgainDescriptionText)
+        case .done:
+            return
+        }
+    }
+    
+    private func setLoadingDescriptionLabelText(text: String) {
+        loadingDescriptionLabel.text = text
+        loadingDescriptionLabel.numberOfLines = 0
+        loadingDescriptionLabel.textAlignment = .center
+    }
+    
+    private func setLoadingTextLabel(text: String) {
+        loadingTextLabel.text = text
+        loadingTextLabel.numberOfLines = 0
+        loadingTextLabel.textAlignment = .center
+    }
 
 	private func setupContstraint() {
 		loadingStackView.pin(
 			.centerX,
 			.centerY
 		)
+        clearNavigationBar.pin(.horizontalEdges(padding: 0), .top(padding: 0))
+        navigationBarDismissButton.pin(.fixedHeight(30), .fixedHeight(30), .top(padding: 22), .trailing(padding: 0))
+        warningTitleImageView.pin(.fixedWidth(56), .fixedHeight(56))
+        tryAgainButton.pin(.horizontalEdges(to: layoutMarginsGuide, padding: 0), .bottom(to: layoutMarginsGuide, padding: 12))
+        speedUpButton.pin(.horizontalEdges(to: layoutMarginsGuide, padding: 0), .bottom(to: layoutMarginsGuide, padding: 12))
+        speedUpDescriptionContainerView.pin(.horizontalEdges(to: layoutMarginsGuide, padding: 0), .relative(.bottom, -24, to:speedUpButton, .top))
+        speedUpDescriptionStackView.pin(.verticalEdges(padding: 10), .leading(padding: 8), .trailing(padding: 16))
+        speedUpDescriptionAlertImageView.pin(.fixedWidth(20), .fixedHeight(20))
 	}
 }
