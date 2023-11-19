0b12b8847340d7926bdbcec663a120c336566a68
sobhaneskandari <eskandari.sobhan@gmail.com>
2023-11-19 14:19:47 +0330

    refactor: web3 helper protocol

--- Pino-iOS/Pino-iOS/Foundation/Web3/Protocols/Web3HelperProtocol.swift
+++ Pino-iOS/Pino-iOS/Foundation/Web3/Protocols/Web3HelperProtocol.swift
@@ -9,37 +9,37 @@ import Foundation
 import Web3
 
 protocol Web3HelperProtocol {
-    var writeWeb3: Web3 { get set }
-    var readWeb3: Web3 { get set }
-    init(writeWeb3: Web3, readWeb3: Web3)
-    var gasInfoManager: W3GasInfoManager { get }
-    var trxManager: W3TransactionManager { get }
-    var transferManager: W3TransferManager { get }
-    var walletManager: PinoWalletManager { get }
-    var userPrivateKey: EthereumPrivateKey { get }
+	var writeWeb3: Web3 { get set }
+	var readWeb3: Web3 { get set }
+	init(writeWeb3: Web3, readWeb3: Web3)
+	var gasInfoManager: W3GasInfoManager { get }
+	var trxManager: W3TransactionManager { get }
+	var transferManager: W3TransferManager { get }
+	var walletManager: PinoWalletManager { get }
+	var userPrivateKey: EthereumPrivateKey { get }
 }
 
 extension Web3HelperProtocol {
-    var gasInfoManager: W3GasInfoManager {
-        .init(writeWeb3: writeWeb3, readWeb3: readWeb3)
-    }
-    
-    var trxManager: W3TransactionManager {
-        .init(writeWeb3: writeWeb3, readWeb3: readWeb3)
-    }
-    
-    var transferManager: W3TransferManager {
-        .init(writeWeb3: writeWeb3, readWeb3: readWeb3)
-    }
-    
-    var walletManager: PinoWalletManager {
-        PinoWalletManager()
-    }
-    
-    var userPrivateKey: EthereumPrivateKey {
-        try! EthereumPrivateKey(
-            hexPrivateKey: walletManager.currentAccountPrivateKey
-                .string
-        )
-    }
+	var gasInfoManager: W3GasInfoManager {
+		.init(writeWeb3: writeWeb3, readWeb3: readWeb3)
+	}
+
+	var trxManager: W3TransactionManager {
+		.init(writeWeb3: writeWeb3, readWeb3: readWeb3)
+	}
+
+	var transferManager: W3TransferManager {
+		.init(writeWeb3: writeWeb3, readWeb3: readWeb3)
+	}
+
+	var walletManager: PinoWalletManager {
+		PinoWalletManager()
+	}
+
+	var userPrivateKey: EthereumPrivateKey {
+		try! EthereumPrivateKey(
+			hexPrivateKey: walletManager.currentAccountPrivateKey
+				.string
+		)
+	}
 }
