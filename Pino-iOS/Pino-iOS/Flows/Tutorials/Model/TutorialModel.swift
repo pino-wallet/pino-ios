812267e112442bf0c00838341640aa7fd5c69b14
sobhaneskandari <eskandari.sobhan@gmail.com>
2023-12-02 14:15:27 +0330

    feat: add lottie library and json animation files

--- Pino-iOS/Pino-iOS/Flows/Tutorials/Model/TutorialModel.swift
+++ Pino-iOS/Pino-iOS/Flows/Tutorials/Model/TutorialModel.swift
@@ -6,49 +6,58 @@
 //
 
 import Foundation
+import Lottie
 
 struct TutorialModel {
 	// MARK: - Public Properties
 
 	public let title: String
 	public let desc: String
-	public let type: TutorialType
+    public let type: TutorialType
+	public let lottieFile: String
 
 	public static let tutorials: [TutorialModel] = [
 		.init(
 			title: "Easy asset conversion",
 			desc: "Easily convert your assets through a minimal swap interface.",
-			type: .swap
+			type: .swap,
+            lottieFile: "SwapEasyAssetConversion"
 		),
 		.init(
 			title: "Easy asset conversion",
 			desc: "Easily convert your assets through a minimal swap interface.",
-			type: .swap
+			type: .swap,
+            lottieFile: "SwapEasyAssetConversion"
 		),
 		.init(
 			title: "Save on Swaps",
 			desc: "Get the best rates for your order from leading DEX aggregators.",
-			type: .swap
+			type: .swap,
+            lottieFile: "SwapSaveOnSwaps"
 		),
 		.init(
 			title: "Loans Made Easy",
 			desc: "Get loans directly through top lending protocols like Compound and Aave.",
-			type: .borrow
+			type: .borrow,
+            lottieFile: "BorrowLoansMadeEasy"
 		),
 		.init(
 			title: "Ensure Loan Safety",
 			desc: "Effortlessly monitor your loan positions to avoid unexpected liquidations.",
-			type: .borrow
+			type: .borrow,
+            lottieFile: "BorrowEnsureLoanSafety"
 		),
 		.init(
 			title: "Simple Yield Earning",
 			desc: "Invest in reliable yield-bearing opportunities and seamlessly manage them.",
-			type: .invest
+			type: .invest,
+            lottieFile: "InvestSimpleYieldEarning"
 		),
 		.init(
 			title: "Easy Profit Tracking",
 			desc: "Track your USD-based investment value and true profit over time.",
-			type: .invest
+			type: .invest,
+            lottieFile: "InvestEasyProfitTracking"
 		),
 	]
 }
