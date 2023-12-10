2f36db092294877aed66f103cb93790fb5f0c57e
Amirrk79 <amirmntmntmntmnt@gmail.com>
2023-12-10 11:44:54 +0330

    fix: remove test user id

--- Pino-iOS/Pino-iOS/Flows/Borrow/ViewModel/BorrowViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/Borrow/ViewModel/BorrowViewModel.swift
@@ -194,9 +194,9 @@ class BorrowViewModel {
 	private func getUserBorrowingDetails() {
 		if collateralizableTokens != nil {
 			borrowAPIClient.getUserBorrowings(
-				address: "0xC6778747F3b685c2FD6Fa5d3883FaDdF37874959",
+                address: walletManager.currentAccount.eip55Address,
 				dex: selectedDexSystem.type
-			).sink { completed in
+			).sink { completed in 
 				switch completed {
 				case .finished:
 					print("User borrowing details received successfully")
