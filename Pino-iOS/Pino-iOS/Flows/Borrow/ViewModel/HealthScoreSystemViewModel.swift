1f38441cd89ef9811a66a0c939af2cef23911b58
Amirrk79 <amirmntmntmntmnt@gmail.com>
2023-12-12 16:57:07 +0330

    feat: healthscore changes

--- Pino-iOS/Pino-iOS/Flows/Borrow/ViewModel/HealthScoreSystemViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/Borrow/ViewModel/HealthScoreSystemViewModel.swift
@@ -10,14 +10,23 @@ struct HealthScoreSystemViewModel {
 	// MARK: - Public Properties
 
 	public var healthScoreNumber: Double
+    public var formattedHealthScore: String {
+         if healthScoreNumber >= 1 && healthScoreNumber < 10 {
+            return String(format: "%.1f", healthScoreNumber)
+        } else if healthScoreNumber >= 10 {
+            return String(format: "%.f", floor(healthScoreNumber))
+        } else {
+            return healthScoreNumber.description
+        }
+    }
 
 	public let healthScoreTitle = "Health Score"
 	public let healthScoreDescription = "This shows how safe your collateral is from liquidation."
 	public let yourScoreText = "Your score"
 	public let liquidationZoneDescription =
-		"Liquidation (Health Score = 0): The protocol can sell your collateral to repay your debt."
-	public let dangerZoneDescribtion = "Danger (0 < Health Score ≤ 10): You are near liquidation."
-	public let safetyZoneDescribtion = "Safety (10 < Health Score < 100): Your position is secure."
+		"Liquidation [0]"
+	public let dangerZoneDescribtion = "Danger [0- 10]"
+	public let safetyZoneDescribtion = "Safety [10 - 100]"
 	public let gotItButtonTitle = "Got it"
 	public let startHealthScoreNumber = "0"
 	public let endHealthScoreNumber = "100"
@@ -25,6 +34,6 @@ struct HealthScoreSystemViewModel {
 	// MARK: - Initializers
 
 	init(healthScoreNumber: Double) {
-		self.healthScoreNumber = healthScoreNumber
+        self.healthScoreNumber = healthScoreNumber
 	}
 }
