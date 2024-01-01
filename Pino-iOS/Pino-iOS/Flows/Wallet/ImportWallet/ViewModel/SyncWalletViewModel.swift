a19cce2b6dc6ad886e5a97106181d88cf30320cb
Amirrk79 <amirmntmntmntmnt@gmail.com>
2024-01-01 13:30:22 +0330

    style: format the code

--- Pino-iOS/Pino-iOS/Flows/Wallet/ImportWallet/ViewModel/SyncWalletViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/Wallet/ImportWallet/ViewModel/SyncWalletViewModel.swift
@@ -8,17 +8,19 @@
 import Foundation
 
 struct SyncWalletViewModel {
-   // MARK: - Public Properties
-    #warning("this image should change to lottie later")
-    public let titleImageName = "sync_wallet"
-    public let titleText = "Synchronizing..."
-    public let descriptionText = "We are syncing your wallet data. This may take a few minutes."
-    public let exploreTitleText = "Do you want to explore Pino in the meantime?"
-    public let explorePinoButtonText = "Explore Pino"
-    public var loadingTime: TimeInterval
-    
-    // MARK: - Initializers
-    init(loadingTime: TimeInterval) {
-        self.loadingTime = loadingTime
-    }
+	// MARK: - Public Properties
+
+	#warning("this image should change to lottie later")
+	public let titleImageName = "sync_wallet"
+	public let titleText = "Synchronizing..."
+	public let descriptionText = "We are syncing your wallet data. This may take a few minutes."
+	public let exploreTitleText = "Do you want to explore Pino in the meantime?"
+	public let explorePinoButtonText = "Explore Pino"
+	public var loadingTime: TimeInterval
+
+	// MARK: - Initializers
+
+	init(loadingTime: TimeInterval) {
+		self.loadingTime = loadingTime
+	}
 }
