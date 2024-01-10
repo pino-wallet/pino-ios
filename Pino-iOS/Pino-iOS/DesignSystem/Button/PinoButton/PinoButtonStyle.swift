5491b814b9ef7c9501ec5f1a6e9d76046929fd91
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2024-01-10 14:43:41 +0330

    feat: add pressed style to pino button

--- Pino-iOS/Pino-iOS/DesignSystem/Button/PinoButton/PinoButtonStyle.swift
+++ Pino-iOS/Pino-iOS/DesignSystem/Button/PinoButton/PinoButtonStyle.swift
@@ -15,9 +15,6 @@ extension PinoButton {
 		public let titleColor: UIColor
 		public let backgroundColor: UIColor
 		public let borderColor: UIColor?
-		public let selectedTitleColor: UIColor
-		public let selectedBackgroundColor: UIColor
-		public let selectedBorderColor: UIColor?
 		public var font: UIFont? = UIFont.PinoStyle.semiboldBody
 	}
 }
@@ -28,73 +25,61 @@ extension PinoButton.Style {
 	public static let active = PinoButton.Style(
 		titleColor: .Pino.white,
 		backgroundColor: .Pino.primary,
-		borderColor: .Pino.clear,
-		selectedTitleColor: .Pino.white,
-		selectedBackgroundColor: .Pino.green3,
-		selectedBorderColor: .Pino.clear
+		borderColor: .Pino.clear
 	)
 
 	public static let deactive = PinoButton.Style(
 		titleColor: .Pino.gray2,
 		backgroundColor: .Pino.gray5,
-		borderColor: .Pino.clear,
-		selectedTitleColor: .Pino.gray2,
-		selectedBackgroundColor: .Pino.gray5,
-		selectedBorderColor: .Pino.clear
+		borderColor: .Pino.clear
 	)
 
 	public static let success = PinoButton.Style(
 		titleColor: .Pino.white,
 		backgroundColor: .Pino.successGreen,
-		borderColor: .Pino.clear,
-		selectedTitleColor: .Pino.white,
-		selectedBackgroundColor: .Pino.successGreen,
-		selectedBorderColor: .Pino.clear
+		borderColor: .Pino.clear
 	)
 
 	public static let delete = PinoButton.Style(
 		titleColor: .Pino.white,
 		backgroundColor: .Pino.errorRed,
-		borderColor: .Pino.clear,
-		selectedTitleColor: .Pino.white,
-		selectedBackgroundColor: .Pino.errorRed,
-		selectedBorderColor: .Pino.clear
+		borderColor: .Pino.clear
 	)
 
 	public static let secondary = PinoButton.Style(
 		titleColor: .Pino.primary,
 		backgroundColor: .Pino.secondaryBackground,
-		borderColor: .Pino.primary,
-		selectedTitleColor: .Pino.green3,
-		selectedBackgroundColor: .Pino.secondaryBackground,
-		selectedBorderColor: .Pino.green3
+		borderColor: .Pino.primary
 	)
 
 	public static let loading = PinoButton.Style(
 		titleColor: .Pino.white,
 		backgroundColor: .Pino.green3,
-		borderColor: .Pino.clear,
-		selectedTitleColor: .Pino.white,
-		selectedBackgroundColor: .Pino.green3,
-		selectedBorderColor: .Pino.clear
+		borderColor: .Pino.clear
 	)
 
 	public static let clear = PinoButton.Style(
 		titleColor: .Pino.primary,
 		backgroundColor: .Pino.clear,
-		borderColor: .Pino.clear,
-		selectedTitleColor: .Pino.green3,
-		selectedBackgroundColor: .Pino.clear,
-		selectedBorderColor: .Pino.clear
+		borderColor: .Pino.clear
 	)
 
 	public static let numpad = PinoButton.Style(
 		titleColor: .Pino.label,
 		backgroundColor: .Pino.clear,
 		borderColor: .Pino.clear,
-		selectedTitleColor: .Pino.secondaryLabel,
-		selectedBackgroundColor: .Pino.clear,
-		selectedBorderColor: .Pino.clear,
 		font: UIFont.PinoStyle.mediumTitle1
+	)
+
+	public static let activeSelected = PinoButton.Style(
+		titleColor: .Pino.white,
+		backgroundColor: .Pino.green3,
+		borderColor: .Pino.clear
+	)
+
+	public static let secondarySelected = PinoButton.Style(
+		titleColor: .Pino.green3,
+		backgroundColor: .Pino.secondaryBackground,
+		borderColor: .Pino.green3
 	)
 }
