fd32b42c1af8a1f82cb228c9ec67a848007e6063
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2024-02-11 12:05:25 +0330

    refactor: add invest risk extension for token id

--- Pino-iOS/Pino-iOS/Flows/Invest/ViewModel/InvestmentBoard/InvestmentRiskViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/Invest/ViewModel/InvestmentBoard/InvestmentRiskViewModel.swift
@@ -128,35 +128,44 @@ enum InvestableAsset: String {
 	public init(assetId: String, investProtocol: InvestProtocolViewModel) {
 		switch investProtocol {
 		case .compound:
-			if assetId.lowercased() == "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".lowercased() {
+			if assetId.lowercased() == TokenId.USDC.lowercased() {
 				self = .USDCCompound
-			} else if assetId.lowercased() == "0xdAC17F958D2ee523a2206206994597C13D831ec7".lowercased() {
+			} else if assetId.lowercased() == TokenId.USDT.lowercased() {
 				self = .USDTCompound
-			} else if assetId.lowercased() == "0x0000000000000000000000000000000000000000".lowercased() {
+			} else if assetId.lowercased() == TokenId.ETH.lowercased() {
 				self = .ETHCompound
 			} else {
 				self = .none
 			}
 		case .aave:
-			if assetId.lowercased() == "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".lowercased() {
+			if assetId.lowercased() == TokenId.USDC.lowercased() {
 				self = .USDCAave
-			} else if assetId.lowercased() == "0xdAC17F958D2ee523a2206206994597C13D831ec7".lowercased() {
+			} else if assetId.lowercased() == TokenId.USDT.lowercased() {
 				self = .USDTAave
 			} else {
 				self = .none
 			}
 		case .maker:
-			if assetId.lowercased() == "0x6B175474E89094C44Da98b954EedeAC495271d0F".lowercased() {
+			if assetId.lowercased() == TokenId.DAI.lowercased() {
 				self = .DAIMaker
 			} else {
 				self = .none
 			}
 		case .lido:
-			if assetId.lowercased() == "0x0000000000000000000000000000000000000000".lowercased() {
+			if assetId.lowercased() == TokenId.ETH.lowercased() {
 				self = .ETHLido
 			} else {
 				self = .none
 			}
 		}
+	}
+}
+
+extension InvestableAsset {
+	public enum TokenId {
+		static let ETH = "0x0000000000000000000000000000000000000000"
+		static let USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+		static let USDT = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+		static let DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F"
 	}
 }
