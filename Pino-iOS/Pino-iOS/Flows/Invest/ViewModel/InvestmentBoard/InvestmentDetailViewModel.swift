2972114fa58511a7c85579109506cb05b80cccc1
Mohadese Raoufi <mhd3.raoufi@gmail.com>
2024-02-24 11:34:40 +0330

    refactor: invest token model

--- Pino-iOS/Pino-iOS/Flows/Invest/ViewModel/InvestmentBoard/InvestmentDetailViewModel.swift
+++ Pino-iOS/Pino-iOS/Flows/Invest/ViewModel/InvestmentBoard/InvestmentDetailViewModel.swift
@@ -56,10 +56,10 @@ class InvestmentDetailViewModel {
 	}
 
 	public var investmentAmount: String {
-		if selectedAsset.investmentCapital < 0.bigNumber {
-			return "-\(selectedAsset.investmentCapital.priceFormat)"
+		if selectedAsset.tokenInvestedAmountInDollar < 0.bigNumber {
+			return "-\(selectedAsset.tokenInvestedAmountInDollar.priceFormat)"
 		} else {
-			return selectedAsset.investmentCapital.priceFormat
+			return selectedAsset.tokenInvestedAmountInDollar.priceFormat
 		}
 	}
 
