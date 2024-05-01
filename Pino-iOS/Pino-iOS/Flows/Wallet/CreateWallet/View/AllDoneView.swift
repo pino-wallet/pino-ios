27cefd718cd20184ee92365b68bdea8f5e0bb04d
Amirrk79 <amirmntmntmntmnt@gmail.com>
2024-05-01 14:09:21 +0330

    style: format the code

--- Pino-iOS/Pino-iOS/Flows/Wallet/CreateWallet/View/AllDoneView.swift
+++ Pino-iOS/Pino-iOS/Flows/Wallet/CreateWallet/View/AllDoneView.swift
@@ -24,16 +24,16 @@ class AllDoneView: UIView {
 	private let agreementLabel = UITextView()
 	private let getStartedButton = PinoButton(style: .deactive)
 	private let hapticManager = HapticManager()
-    private let navigationBarHeight: CGFloat
+	private let navigationBarHeight: CGFloat
 	private var getStarted: () -> Void
 	private var allDoneVM: AllDoneViewModel
 
 	// MARK: - Initializers
 
-    init(allDoneVM: AllDoneViewModel, getStarted: @escaping (() -> Void), navigationBarHeight: CGFloat) {
+	init(allDoneVM: AllDoneViewModel, getStarted: @escaping (() -> Void), navigationBarHeight: CGFloat) {
 		self.getStarted = getStarted
 		self.allDoneVM = allDoneVM
-        self.navigationBarHeight = navigationBarHeight
+		self.navigationBarHeight = navigationBarHeight
 		super.init(frame: .zero)
 		setupView()
 		setupStyle()
@@ -127,8 +127,8 @@ extension AllDoneView {
 		)
 		allDoneAnimationView.pin(
 			.horizontalEdges(to: safeAreaLayoutGuide),
-            .bottom(to: safeAreaLayoutGuide),
-            .top(to: safeAreaLayoutGuide, padding: -navigationBarHeight)
+			.bottom(to: safeAreaLayoutGuide),
+			.top(to: safeAreaLayoutGuide, padding: -navigationBarHeight)
 		)
 		allDoneTitleImageView.pin(.fixedWidth(80), .fixedHeight(80))
 		getStartedButton.pin(
